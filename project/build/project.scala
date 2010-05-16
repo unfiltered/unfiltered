2cbbbd484083508f78e739126c796012baf04f29
Nathan Hamblen <nathan@technically.us>
2010-05-16 10:57:48 -0400

    jetty in test

--- project/build/project.scala
+++ project/build/project.scala
@@ -2,4 +2,6 @@ import sbt._
 
 class Unfiltered(info: ProjectInfo) extends DefaultProject(info) {
   val servlet_api = "javax.servlet" % "servlet-api" % "2.3" % "provided"
+
+  val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.v20100331" % "test"
 }
