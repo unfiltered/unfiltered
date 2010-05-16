4dcbb5da88259926f60b3dead65fc31b1482b8cb
Nathan Hamblen <nathan@technically.us>
2010-05-16 19:21:31 -0400

    api -> library

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 class Unfiltered(info: ProjectInfo) extends ParentProject(info) {
-  lazy val api = project("api", "Unfiltered", new DefaultProject(_) {
+  lazy val library = project("library", "Unfiltered", new DefaultProject(_) {
     val servlet_api = "javax.servlet" % "servlet-api" % "2.3" % "provided"
   })
   lazy val server = project("server", "Unfiltered Server", new DefaultProject(_) {
     val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.v20100331"
-  }, api)
+  }, library)
   lazy val demo = project("demo", "Unfiltered Demo", new DefaultProject(_), server)
 }
