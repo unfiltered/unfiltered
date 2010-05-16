35dca614308db039e9f9ca531337cb4235640114
Nathan Hamblen <nathan@technically.us>
2010-05-16 17:34:45 -0400

    api and server projects

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,7 +1,10 @@
 import sbt._
 
-class Unfiltered(info: ProjectInfo) extends DefaultProject(info) {
-  val servlet_api = "javax.servlet" % "servlet-api" % "2.3" % "provided"
-
-  val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.v20100331" % "test"
+class Unfiltered(info: ProjectInfo) extends ParentProject(info) {
+  lazy val api = project("api", "Unfiltered", new DefaultProject(_) {
+    val servlet_api = "javax.servlet" % "servlet-api" % "2.3" % "provided"
+  })
+  lazy val server = project("server", "Unfiltered Server", new DefaultProject(_) {
+    val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.v20100331"
+  }, api)
 }
