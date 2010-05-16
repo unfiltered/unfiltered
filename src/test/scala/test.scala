aaba550227c60d9e1b30c32d4e6addfebccd4363
Nathan Hamblen <nathan@technically.us>
2010-05-15 20:19:04 -0400

    regex tricks

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -1,6 +1,10 @@
 import unfiltered.request._
 import unfiltered.response._
 
+object AdminId extends scala.util.matching.Regex(
+  "/admin/(%d)+"
+)
+
 class Test extends unfiltered.Handler ({
-  case HTTPS(GET(Path("/admin", req))) => Pass
+  case HTTPS(GET(Path(AdminId(id), req))) => Pass
 })
