41661fe326a77ecd8939b62c8b38415e82b050be
Nathan Hamblen <nathan@technically.us>
2010-05-19 00:31:16 -0400

    sxr publish

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,17 +1,19 @@
 import sbt._
 
 class Unfiltered(info: ProjectInfo) extends ParentProject(info) {
-  lazy val library = project("library", "Unfiltered", new DefaultProject(_) {
+  class UnfilteredModule(info: ProjectInfo) extends DefaultProject(info) with sxr.Publish
+  
+  lazy val library = project("library", "Unfiltered", new UnfilteredModule(_) {
     val servlet_api = "javax.servlet" % "servlet-api" % "2.3" % "provided"
   })
   val jetty_version = "7.0.2.v20100331"
-  lazy val server = project("server", "Unfiltered Server", new DefaultProject(_) {
+  lazy val server = project("server", "Unfiltered Server", new UnfilteredModule(_) {
     val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % jetty_version
   }, library)
-  lazy val ajp_server = project("ajp-server", "Unfiltered AJP Server", new DefaultProject(_) {
+  lazy val ajp_server = project("ajp-server", "Unfiltered AJP Server", new UnfilteredModule(_) {
     val jetty7 = "org.eclipse.jetty" % "jetty-ajp" % jetty_version
   }, server)
-  lazy val demo = project("demo", "Unfiltered Demo", new DefaultProject(_), server)
+  lazy val demo = project("demo", "Unfiltered Demo", new UnfilteredModule(_), server)
 
   /** Exclude demo from publish, all other actions run from parent */
   override def dependencies = super.dependencies.filter { d => !(d eq demo) }
