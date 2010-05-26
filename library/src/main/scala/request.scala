82155c09307dfc3573121aeeb4d36a667304660a
Nathan Hamblen <nathan@technically.us>
2010-05-26 08:09:37 -0400

    request body extractors

--- library/src/main/scala/request.scala
+++ library/src/main/scala/request.scala
@@ -44,10 +44,29 @@ class RequestHeader(name: String) {
 object IfNoneMatch extends RequestHeader("If-None-Match")
 
 
-object RequestStream {
+object InStream {
   def unapply(req: HttpServletRequest) = Some(req.getInputStream, req)
 }
 
-object RequestReader {
+object Read {
   def unapply(req: HttpServletRequest) = Some(req.getReader, req)
+}
+
+object Bytes {
+  def unapply(req: HttpServletRequest) = {
+    val InStream(in, _) = req
+    val bos = new java.io.ByteArrayOutputStream
+    val ba = new Array[Byte](4096)
+    /* @scala.annotation.tailrec */ def read {
+      val len = in.read(ba)
+      if (len > 0) bos.write(ba, 0, len)
+      if (len >= 0) read
+    }
+    read
+    in.close
+    bos.toByteArray match {
+      case Array() => None
+      case ba => Some(ba)
+    }
+  }
 }
