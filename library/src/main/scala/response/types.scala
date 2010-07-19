ecef66c78ca9147cef481c9fc92bf6b92392bcd7
doug tangren <d.tangren@gmail.com>
2010-07-19 01:29:12 -0400

    adding a few more common content types

--- library/src/main/scala/response/types.scala
+++ library/src/main/scala/response/types.scala
@@ -10,3 +10,9 @@ case class ContentType(content_type: String) extends Responder {
 }
 object CssContent extends ContentType("text/css")
 object HtmlContent extends ContentType("text/html")
+object JsContent extends ContentType("text/javascript")
+object CsvContent extends ContentType("text/csv")
+object TextXmlContent extends ContentType("text/xml")
+object PlainTextContent extends ContentType("text/plain")
+object JsonContent extends ContentType("application/json")
+object ApplicationXmlContent extends ContentType("application/xml")
