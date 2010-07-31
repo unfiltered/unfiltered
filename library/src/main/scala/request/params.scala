97e7684658b255f3a2a9ceca89f965966e7a4218
Nathan Hamblen <nathan@technically.us>
2010-07-31 15:10:10 -0400

    putting params back to master's copy

--- library/src/main/scala/request/params.scala
+++ library/src/main/scala/request/params.scala
@@ -2,8 +2,6 @@ package unfiltered.request
 
 import javax.servlet.http.HttpServletRequest
 
-case class FileItem(name: String, content: Array[Byte], contentType: String)
-
 object Params {
   /** Dress a Java Enumeration in Scala Iterator clothing */
   case class JEnumerationIterator[T](e: java.util.Enumeration[T]) extends Iterator[T] {
@@ -16,34 +14,10 @@ object Params {
     is no such parameter, or (as normal for servlets) a single empty string if the
     parameter was supplied without a value. */
   def unapply(req: HttpServletRequest) = {
-    import org.apache.commons.{fileupload => fu}
-    import fu.{FileItemFactory}
-    import fu.disk.{DiskFileItemFactory}
-    import fu.servlet.{ServletFileUpload}
-    
-    if (ServletFileUpload.isMultipartContent(req)) {
-      val factory = new DiskFileItemFactory(Int.MaxValue, new java.io.File("."))
-      val upload = new ServletFileUpload(factory)
-      val items = upload.parseRequest(req).toArray.toList collect {
-        case x: fu.FileItem => (x.getFieldName, x) }
-      val names = (items map { item => item._1 }).distinct
-      
-      Some((((Map.empty[String, Seq[String]] /: names) ((m, n) => 
-          m + (n -> (items filter { _._1 == n } map { _._2.getString }) )
-        )).withDefaultValue(Nil),
-        ((Map.empty[String, Seq[FileItem]] /: names) ((m, n) => 
-            m + (n -> (items filter { x => x._1 == n && !x._2.isFormField } map { x =>
-              FileItem(x._2.getName, x._2.get, x._2.getContentType) }) )
-          )).withDefaultValue(Nil),
-        req))
-    } else {
-      val names = JEnumerationIterator[String](req.getParameterNames.asInstanceOf[java.util.Enumeration[String]])
-      Some(((Map.empty[String, Seq[String]] /: names) ((m, n) => 
-          m + (n -> req.getParameterValues(n))
-        )).withDefaultValue(Nil),
-        Map.empty[String, Seq[FileItem]],
-        req)     
-    }
+    val names = JEnumerationIterator[String](req.getParameterNames.asInstanceOf[java.util.Enumeration[String]])
+    Some(((Map.empty[String, Seq[String]] /: names) ((m, n) => 
+        m + (n -> req.getParameterValues(n))
+      )).withDefaultValue(Nil), req)
   }
 
   abstract class Named[T](name: String, f: Seq[String] => Option[T]) {
