4bc5efd2b75fcac95c82dca740ed3291f40abf0b
dwhitney <dustin.whitney@gmail.com>
2010-08-03 22:06:14 -0400

    added a work around for a test that needs the scala compiler, and adjusted spacing

--- scalate/src/main/scala/Scalate.scala
+++ scalate/src/main/scala/Scalate.scala
@@ -6,7 +6,7 @@ import org.fusesource.scalate.{TemplateEngine, Binding}
  * private object that holds the default engine
 **/
 private[scalate] object ScalateDefaults{
-    implicit val engine = new TemplateEngine
+  implicit val engine = new TemplateEngine
 }
 
 import unfiltered.response.ResponseWriter
@@ -15,24 +15,20 @@ This class will render the given template with the given attributes.
 An implicit Engine and Bindings can be passed to the constructor via
 its alternate parameters set.  This is described further in demo-scalate
 **/
-case class Scalate
-        (
-            template: String, 
-            attributes:(String,Any)*
-        )
-        (
-            implicit engine: TemplateEngine = ScalateDefaults.engine, 
-            bindings: List[Binding] = List[Binding](),
-            additionalAttributes: List[(String, Any)] = List[(String, Any)]()
-        ) extends ResponseWriter{
+case class Scalate(template: String, attributes:(String,Any)*)
+  (
+    implicit engine: TemplateEngine = ScalateDefaults.engine, 
+    bindings: List[Binding] = List[Binding](),
+    additionalAttributes: List[(String, Any)] = List[(String, Any)]()
+  ) extends ResponseWriter{
 
-    import java.io.PrintWriter
-    import org.fusesource.scalate.DefaultRenderContext
-    def write(writer: PrintWriter): Unit = {
-        val scalateTemplate = engine.load(template, bindings)
-        val context = new DefaultRenderContext(engine, writer)
-        for(attr <- additionalAttributes) context.attributes(attr._1) = attr._2
-        for(attr <- attributes) context.attributes(attr._1) = attr._2 
-        scalateTemplate.render(context)
-    }
+  import java.io.PrintWriter
+  import org.fusesource.scalate.DefaultRenderContext
+  def write(writer: PrintWriter): Unit = {
+    val scalateTemplate = engine.load(template, bindings)
+    val context = new DefaultRenderContext(engine, writer)
+    for(attr <- additionalAttributes) context.attributes(attr._1) = attr._2
+    for(attr <- attributes) context.attributes(attr._1) = attr._2 
+    scalateTemplate.render(context)
+  }
 }
--- scalate/src/test/scala/ScalateSpec.scala
+++ scalate/src/test/scala/ScalateSpec.scala
@@ -13,39 +13,45 @@ import org.fusesource.scalate.support.FileResourceLoader
 
 class ScalateSpec extends FlatSpec with MustMatchers {
 	
-	"A Template" should "load" in {
-        val scalate = Scalate("scalate/src/test/resources/hello.ssp")
+  "A Template" should "load" in {
+    val scalate = Scalate("scalate/src/test/resources/hello.ssp")
         
-        val buffer = new StringWriter
-        val writer = new PrintWriter(buffer)
-        scalate.write(writer)
+    val buffer = new StringWriter
+    val writer = new PrintWriter(buffer)
+    scalate.write(writer)
         
-        buffer.toString must equal ("<h1>Hello, World!</h1>")
-    }
+    buffer.toString must equal ("<h1>Hello, World!</h1>")
+  }
     
-    it should "accept an implicit engine" in {
-        implicit val myEngine = new TemplateEngine
-        myEngine.resourceLoader = new FileResourceLoader(Some(new File("./scalate/src/test/resources/alternate/")))
-        val scalate = Scalate("another_test_template.ssp")
+  it should "accept an implicit engine" in {
+    implicit val myEngine = new TemplateEngine
+    myEngine.resourceLoader = new FileResourceLoader(Some(new File("./scalate/src/test/resources/alternate/")))
+    val scalate = Scalate("another_test_template.ssp")
         
-        val buffer = new StringWriter
-        val writer = new PrintWriter(buffer)
-        scalate.write(writer)
+    val buffer = new StringWriter
+    val writer = new PrintWriter(buffer)
+    scalate.write(writer)
         
-        buffer.toString must equal ("<h1>Another Template!</h1>")
-    }
+    buffer.toString must equal ("<h1>Another Template!</h1>")
+  }
     
-    it should "accept implicit bindings" in {
-        implicit val bindings: List[Binding] = List(Binding(name = "foo", className = "String"))
-        implicit val additionalAttributes = List(("foo", "bar"))
+  it should "accept implicit bindings" in {
+    implicit val bindings: List[Binding] = List(Binding(name = "foo", className = "String"))
+    implicit val additionalAttributes = List(("foo", "bar"))
         
-        val scalate = Scalate("scalate/src/test/resources/bindings.ssp")
+    val scalate = Scalate("scalate/src/test/resources/bindings.ssp")
         
-        val buffer = new StringWriter
-        val writer = new PrintWriter(buffer)
-        scalate.write(writer)
+    val buffer = new StringWriter
+    val writer = new PrintWriter(buffer)
+    scalate.write(writer)
         
-        buffer.toString must equal ("bar")
-    }
+    buffer.toString must equal ("bar")
+  }
+
+  //sbt will not put the Scala compiler onto the classpath unless an explicit reference is made
+  private def loadTheScalaCompilerOntoTheClasspath = {
+    import scala.tools.nsc._
+    new Global(new Settings)
+  }
 
 }
