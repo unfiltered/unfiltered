1cff68193f51b1fc5abe65c5152e5bc273678e33
Nathan Hamblen <nathan@technically.us>
2010-08-04 00:18:06 -0400

    move tests out of server module

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,15 +1,18 @@
 import sbt._
 
 class Unfiltered(info: ProjectInfo) extends ParentProject(info) {
-  
-  class UnfilteredModule(info: ProjectInfo) extends DefaultProject(info) with sxr.Publish {
-    lazy val unfilteredSpec =  "net.databinder" %% "unfiltered-spec" % "0.1.3" % "test" intransitive()
+
+  class UnfilteredModule(info: ProjectInfo) extends DefaultProject(info) with sxr.Publish
+
+  trait IntegrationTesting extends BasicDependencyProject {
+    lazy val unfilteredSpec =  "net.databinder" %% "unfiltered-spec" % "0.1.3" % "test"
     lazy val specs = specsDependency % "test"
     lazy val dispatch = dispatchDependency % "test"
   }
-  
+
   /** core unfiltered library */
-  lazy val library = project("library", "Unfiltered", new UnfilteredModule(_) {
+  lazy val library = project("library", "Unfiltered",
+      new UnfilteredModule(_) with IntegrationTesting {
     lazy val servlet_api = servletApiDependency
     val codec = "commons-codec" % "commons-codec" % "1.4"
   })
@@ -17,13 +20,13 @@ class Unfiltered(info: ProjectInfo) extends ParentProject(info) {
   lazy val uploads = project("uploads", "Unfiltered Uploads", new UnfilteredModule(_) {
     lazy val servlet_api = servletApiDependency
     val io = "commons-io" % "commons-io" % "1.4"
-    val fileupload = "commons-fileupload" % "commons-fileupload" % "1.2.1"  
-  })
+    val fileupload = "commons-fileupload" % "commons-fileupload" % "1.2.1"
+  }, library)
   val jetty_version = "7.0.2.v20100331"
   /** embedded server*/
   lazy val server = project("server", "Unfiltered Server", new UnfilteredModule(_) {
     val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % jetty_version
-  }, library, uploads)
+  }, library)
   /** AJP protocol server */
   lazy val ajp_server = project("ajp-server", "Unfiltered AJP Server", new UnfilteredModule(_) {
     val jetty7 = "org.eclipse.jetty" % "jetty-ajp" % jetty_version
@@ -36,19 +39,20 @@ class Unfiltered(info: ProjectInfo) extends ParentProject(info) {
     lazy val dispatch = dispatchDependency
   }, server)
   /** json extractors */
-  lazy val json = project("json", "Unfiltered Json", new DefaultProject(_) with sxr.Publish {
+  lazy val json = project("json", "Unfiltered Json", 
+      new UnfilteredModule(_) with IntegrationTesting {
     val dispatch_json = "net.databinder" %% "dispatch-json" % "0.7.4"
     val test_server = "net.databinder" %% "unfiltered-server" % "0.1.3" % "test"
   }, library)
-  
+
   def servletApiDependency = "javax.servlet" % "servlet-api" % "2.3" % "provided"
-  def specsDependency = 
-    if (buildScalaVersion startsWith "2.7.") 
+  def specsDependency =
+    if (buildScalaVersion startsWith "2.7.")
       "org.scala-tools.testing" % "specs" % "1.6.2.2"
     else
       "org.scala-tools.testing" %% "specs" % "1.6.5"
   def dispatchDependency = "net.databinder" %% "dispatch-mime" % "0.7.4"
-  
+
   /** Exclude demo from publish, all other actions run from parent */
   override def dependencies = super.dependencies.filter { d => !(d eq demo) }
 
