df14d70ae4b893baf81bd5bd57450fa9e1aaa14f
Nathan Hamblen <nathan@technically.us>
2010-08-15 11:21:02 -0400

    latest sxr publish

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -3,6 +3,6 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
   val snuggletex_repo = "snuggletex_repo" at "http://www2.ph.ed.ac.uk/maven2"
-  val sxr_publish = "net.databinder" % "sxr-publish" % "0.1.9"
+  val sxr_publish = "net.databinder" % "sxr-publish" % "0.2.0"
   val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
 }
