e6ea11fdc1029ba99cd2734c8d78557bc32800f3
chrislewis <chris@rosaloves.com>
2010-08-19 21:46:47 -0400

    further mixed-header coverage, tip-toeing around the match orders

--- library/src/test/scala/AcceptsSpec.scala
+++ library/src/test/scala/AcceptsSpec.scala
@@ -23,6 +23,10 @@ object AcceptSpec extends Specification  with unfiltered.spec.Served {
       val resp = Http(host / "test" <:< Map("Accept" -> "application/json")  as_str)
       resp must_=="json"
     }
+    "match a mixed accepts request with json as json" in {
+      val resp = Http(host / "test" <:< Map("Accept" -> "application/xhtml+xml,text/xml;q=0.5,application/xml;q=0.9,*/*;q=0.8,application/json")  as_str)
+      resp must_=="json"
+    }
     "match a .json file extension as json when accepts is empty or contains a wildcard" in {
       val resp = Http(host / "test.json" <:< Map("Accept" -> "*/*") as_str)
       resp must_=="json"
@@ -31,8 +35,8 @@ object AcceptSpec extends Specification  with unfiltered.spec.Served {
       val resp = Http(host / "test" <:< Map("Accept" -> "text/xml")  as_str)
       resp must_=="xml"
     }
-    "match a complex accepts request as xml" in {
-      val resp = Http(host / "test" <:< Map("Accept" -> "application/xhtml+xml,text/xml;q=0.5,application/xml;q=0.9,*/*;q=0.8")  as_str)
+    "match a mixed accepts request with xml as xml" in {
+      val resp = Http(host / "test" <:< Map("Accept" -> "text/html,application/xhtml+xml,text/xml;q=0.5,application/xml;q=0.9,*/*;q=0.8")  as_str)
       resp must_=="xml"
     }
     "match a .xml file extension as json when accepts is empty or contains a wildcard" in {
@@ -43,12 +47,20 @@ object AcceptSpec extends Specification  with unfiltered.spec.Served {
       val resp = Http(host / "test" <:< Map("Accept" -> "text/html")  as_str)
       resp must_=="html"
     }
+    "match a mixed accepts request with html as html" in {
+      val resp = Http(host / "test" <:< Map("Accept" -> "application/octet-stream,text/html,application/pdf;q=0.9,*/*;q=0.8")  as_str)
+      resp must_=="html"
+    }
     "match a .html file extension as html when accepts is empty or contains a wildcard" in {
       val resp = Http(host / "test.html" <:< Map("Accept" -> "*/*")  as_str)
       resp must_=="html"
     }
     "match a text/csv accepts request as csv" in {
       val resp = Http(host / "test" <:< Map("Accept" -> "text/csv")  as_str)
+      resp must_=="csv"
+    }
+    "match a mixed accepts request with csv as csv" in {
+      val resp = Http(host / "test" <:< Map("Accept" -> "text/html,text/csv,application/pdf;q=0.9,*/*;q=0.8")  as_str)
       resp must_=="csv"
     }
     "match a .csv file extension as csv when accepts is empty or contains a wildcard" in {
