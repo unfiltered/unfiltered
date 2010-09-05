dc561f0c97936a105d4b7baefb388bdb272796bb
Nathan Hamblen <nathan@technically.us>
2010-09-05 12:00:05 -0400

    afterStart for Server#run

--- server/src/main/scala/server.scala
+++ server/src/main/scala/server.scala
@@ -44,17 +44,24 @@ trait Server extends ContextBuilder {
   }
   lazy val current = contextHandler("/")
   
-  /** Runs the server and joins its controlling thread. If the current thread is not the main thread, 
-      e.g. if running in sbt, waits for input in a loop and stops the server as soon as any key is pressed.  */
+  /** Calls run with a no-op afterStart */
   def run() {
+    run { _ => () }
+  }
+  /** Starts the server, calls andThen, and joins the server's controlling thread. If the
+   * current thread is not the main thread, e.g. if running in sbt, waits for input in a
+   * loop and stops the server as soon as any key is pressed.  */
+  def run(afterStart: this.type => Unit) {
     // enter wait loop if not in main thread, e.g. running inside sbt
     Thread.currentThread.getName match {
       case "main" => 
         server.setStopAtShutdown(true)
         server.start()
+        afterStart(Server.this)
         server.join()
       case _ => 
         server.start()
+        afterStart(Server.this)
         println("Embedded server running. Press any key to stop.")
         def doWait() {
           try { Thread.sleep(1000) } catch { case _: InterruptedException => () }
