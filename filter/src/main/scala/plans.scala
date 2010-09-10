109521e16273d95bf58529ef184fa6fbedda99f7
Nathan Hamblen <nathan@technically.us>
2010-09-09 23:53:35 -0400

    restore orig plans

--- filter/src/main/scala/plans.scala
+++ filter/src/main/scala/plans.scala
@@ -1,37 +1,44 @@
 package unfiltered
 
-import response._
-import request._
-import response.ResponsePackage.ResponseFunction
+import javax.servlet.{Filter, FilterConfig, FilterChain, ServletRequest, ServletResponse}
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import unfiltered.response.{ResponsePackage, Pass, PassAndThen}
+import ResponsePackage.ResponseFunction
 
+trait InittedFilter extends Filter {
+  private var config_var: FilterConfig = _
+  def init(config: FilterConfig) { config_var = config; }
+  def config = config_var
 
+  def destroy { }
+}
 
 /** To ecapsulate a filter in a class definition */
-trait Plan  {
-  def filter: PartialFunction[HttpRequest, ResponseFunction]
-
-  /*def doFilter(request: HttpRequest, response: HttpResponse, chain: FilterChain) {
+trait Plan extends InittedFilter {
+  def filter: PartialFunction[ServletRequest, ResponseFunction]
+  
+  def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
     (request, response) match {
-      case (request: HttpServletRequest, response: HttpHttpResponse) =>
+      case (request: HttpServletRequest, response: HttpServletResponse) => 
         (try {
           filter(request)
         } catch {
-          case m: MatchError =>
+          case m: MatchError => 
             Pass
         }) match {
           case after: PassAndThen =>
             chain.doFilter(request, response)
             after.then(request)(response)
           case Pass => chain.doFilter(request, response)
-          case response_function => response_function(response)
+          case response_function => response_function(response) 
         }
      }
-  }*/
+  }
 }
 
 /** To define a filter class with an independent function */
-class Planify(val filter: PartialFunction[HttpRequest, ResponseFunction]) extends Plan
+class Planify(val filter: PartialFunction[ServletRequest, ResponseFunction]) extends Plan
 /** To create a filter instance with an independent function */
 object Planify {
-  def apply(filter: PartialFunction[HttpRequest, ResponseFunction]) = new Planify(filter)
+  def apply(filter: PartialFunction[ServletRequest, ResponseFunction]) = new Planify(filter)
 }
