30a410c1b0f858e0e7958f9c89669c8a508d9eff
Nathan Hamblen <nathan@technically.us>
2010-09-26 11:01:46 -0400

    desktop browser util

---
+++ library/src/main/scala/utils.scala
@@ -0,0 +1,24 @@
+package unfiltered
+
+object Port {
+  /** Finds any available port and returns it */
+  def any = {
+    val s = new java.net.ServerSocket(0)
+    val p = s.getLocalPort
+    s.close()
+    p
+  }
+}
+
+object Browser {
+  /** Tries to open a browser window, returns Some(exception) on failure */
+  def open(loc: String) =
+    try {
+      import java.net.URI
+      val dsk = Class.forName("java.awt.Desktop")
+      dsk.getMethod("browse", classOf[URI]).invoke(
+        dsk.getMethod("getDesktop").invoke(null), new URI(loc)
+      )
+      None
+    } catch { case e => Some(e) }
+}
