45dd0c8cbe5a3cf0bf137e78ab265d11b31fe9eb
softprops <d.tangren@gmail.com>
2010-10-01 01:22:55 -0400

    making server a little more extendable

--- netty/src/main/scala/servers.scala
+++ netty/src/main/scala/servers.scala
@@ -6,16 +6,19 @@ import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory
 import java.net.InetSocketAddress
 import org.jboss.netty.handler.codec.http.{HttpRequestDecoder, HttpResponseEncoder}
 import org.jboss.netty.channel._
-import group.DefaultChannelGroup
+import group.{ChannelGroup, DefaultChannelGroup}
 
-class Server(val port: Int, lastHandler: ChannelHandler) {
+class Server(val port: Int, val lastHandler: ChannelHandler) {
   val DEFAULT_IO_THREADS = Runtime.getRuntime().availableProcessors() + 1;
   val DEFAULT_EVENT_THREADS = DEFAULT_IO_THREADS * 4;
-
-  private var socketChannel : Channel = _
+  
   private var bootstrap: ServerBootstrap = _
-
-  private val pipelineFactory = new ServerPipelineFactory(lastHandler)
+  
+  /** any channels added to this will receive broadcasted events */
+  protected val channels = new DefaultChannelGroup("Netty Unfiltered Server Channel Group")
+  
+  /** override this to provide an alternative pipeline factory */
+  protected lazy val pipelineFactory: ChannelPipelineFactory = new ServerPipelineFactory(channels, lastHandler)
 
   def start() = {
 
@@ -32,27 +35,22 @@ class Server(val port: Int, lastHandler: ChannelHandler) {
     bootstrap.setOption("sendBufferSize", 128 * 1024)
     bootstrap.setOption("reuseAddress", true)
     bootstrap.setOption("backlog", 16384)
-    socketChannel = bootstrap.bind(new InetSocketAddress(port))
+    channels.add(bootstrap.bind(new InetSocketAddress(port)))
   }
 
   def stop() = {
-    // Unbind the server socket
-    socketChannel.unbind.awaitUninterruptibly
-    // Close any pending connections / channels
-    pipelineFactory.houseKeeping.channelGroup.close.awaitUninterruptibly
+    // Close any pending connections / channels (including server)
+    channels.close.awaitUninterruptibly
     // Release NIO resources to the OS
     bootstrap.releaseExternalResources
   }
 }
 
-class ServerPipelineFactory(val lastHandler: ChannelHandler) extends ChannelPipelineFactory {
-
-  val houseKeeping = new HouseKeepingChannelHandler
-
+class ServerPipelineFactory(channels: ChannelGroup, lastHandler: ChannelHandler) extends ChannelPipelineFactory {
   def getPipeline(): ChannelPipeline = {
     val line = Channels.pipeline
 
-    line.addLast("houskeeping", houseKeeping)
+    line.addLast("houskeeping", new HouseKeepingChannelHandler(channels))
     line.addLast("decoder", new HttpRequestDecoder)
     line.addLast("encoder", new HttpResponseEncoder)
     line.addLast("handler", lastHandler)
@@ -65,12 +63,9 @@ class ServerPipelineFactory(val lastHandler: ChannelHandler) extends ChannelPipe
  * Channel handler that keeps track of channels in a ChannelGroup for controlled
  * shutdown.
  */
-class HouseKeepingChannelHandler extends SimpleChannelUpstreamHandler {
-  val channelGroup = new DefaultChannelGroup("Netty Unfiltered Channel group")
-
+class HouseKeepingChannelHandler(channels: ChannelGroup) extends SimpleChannelUpstreamHandler {
   override def channelOpen(ctx: ChannelHandlerContext, e: ChannelStateEvent) = {
     // Channels are automatically removed from the group on close
-    channelGroup.add(e.getChannel)
+    channels.add(e.getChannel)
   }
-
 }
