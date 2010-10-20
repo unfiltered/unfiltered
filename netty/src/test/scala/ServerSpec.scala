cd8172d771fc0b58a24ea348e2acbdd75296b933
Nathan Hamblen <nathan@technically.us>
2010-10-19 22:22:14 -0400

    whitespace

--- netty/src/test/scala/ServerSpec.scala
+++ netty/src/test/scala/ServerSpec.scala
@@ -7,9 +7,9 @@ import unfiltered.request.{Path => UFPath}
 object NettyServerTest {
   def main(args: Array[String]) = {
 
-    val p = unfiltered.netty.cycle.Planify( {
-        case GET(UFPath("/", _)) => ResponseString("test") ~> Ok
-      })
+    val p = unfiltered.netty.cycle.Planify({
+      case GET(UFPath("/", _)) => ResponseString("test") ~> Ok
+    })
 
     Server(8080, p).start
   }
