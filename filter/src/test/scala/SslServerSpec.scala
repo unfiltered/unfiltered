d06bccc42a412139356b31f883592e5240b07d56
softprops <d.tangren@gmail.com>
2010-10-26 22:12:30 -0400

    passing jetty ssl server test, keystore now uses 'localhost' as domain

--- filter/src/test/scala/SslServerSpec.scala
+++ filter/src/test/scala/SslServerSpec.scala
@@ -1,8 +1,9 @@
 package unfiltered.server
 
+import unfiltered.spec
 import org.specs._
 
-object SslServerSpec extends Specification with unfiltered.spec.jetty.Served {
+object SslServerSpec extends Specification with spec.jetty.Served with spec.SecureClient {
   import unfiltered.response._
   import unfiltered.request._
   import unfiltered.request.{Path => UFPath}
@@ -11,12 +12,16 @@ object SslServerSpec extends Specification with unfiltered.spec.jetty.Served {
   
   import dispatch._
   
-  override val host = :/("localhost", 8443)
+  val keyStorePath = getClass.getResource("/keystore").getPath
+  val keyStorePasswd = "unfiltered"
+  val securePort = 8443
+  
+  override val host = :/("localhost", securePort)
   
   override lazy val server = setup(new JHttp(port) with Ssl {
-    def sslPort = 8443
-    override lazy val keyStore = getClass.getResource("/keystore").getPath
-    override lazy val keyStorePassword = "unfiltered"
+    def sslPort = securePort
+    override lazy val keyStore = keyStorePath
+    override lazy val keyStorePassword = keyStorePasswd
   })
   
   def setup = { _.filter(unfiltered.filter.Planify {
@@ -25,10 +30,10 @@ object SslServerSpec extends Specification with unfiltered.spec.jetty.Served {
   
   "A Secure Server" should {
     "respond to secure requests" in {
-      Http(host.secure as_str) must_== "secret"
+      http(host.secure as_str) must_== "secret"
     }
     "refuse connection to unsecure requests" in {
-      Http(host as_str) must throwA[ClientProtocolException]
+      http(host as_str) must throwA[ClientProtocolException]
     }
   }
 }
