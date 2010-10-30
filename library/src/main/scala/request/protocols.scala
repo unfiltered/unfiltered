9e68372e5261a2b9adbf731f17ffbfdc3cc7ea61
softprops <d.tangren@gmail.com>
2010-10-30 01:15:37 -0400

    protocol has little to do with https and more to do with the version. add an extractor for that instead

--- library/src/main/scala/request/protocols.scala
+++ library/src/main/scala/request/protocols.scala
@@ -1,7 +1,9 @@
 package unfiltered.request
 
-object HTTPS {
+abstract class Protocol(p: String) {
   def unapply[T](req: HttpRequest[T]) = 
-    if (req.protocol.equalsIgnoreCase("HTTPS")) Some(req)
+    if (req.protocol.equalsIgnoreCase(p)) Some(req)
     else None
 }
+object HTTP_1_0 extends Protocol("HTTP/1.0")
+object HTTP_1_1 extends Protocol("HTTP/1.1")
