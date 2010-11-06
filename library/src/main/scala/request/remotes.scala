cb509087d9406015910a94c6d089f108d07c7e22
softprops <d.tangren@gmail.com>
2010-11-06 13:23:12 -0400

    remote unneeded use of implicity conversion

--- library/src/main/scala/request/remotes.scala
+++ library/src/main/scala/request/remotes.scala
@@ -1,11 +1,6 @@
 package unfiltered.request
 
 object RemoteAddr {
-  // filterNot shim for scala 2.7.7 line
-  class FilterNotList[T](l:List[T]) {
-    def filterNot(p: T => Boolean) = l.filter(!p(_))
-  }
-  implicit def l2fnl[T](l: List[T]) = new FilterNotList(l)
   /** @see http://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces 
     * includes private trusted addresses
     * 127.0.0.1 (localhost)
@@ -16,7 +11,7 @@ object RemoteAddr {
   val TrustedProxies = """(^127\.0\.0\.1$|^(10|172\.(1[6-9]|2[0-9]|3[0-1])|192\.168)\.\S+)""".r
   def unapply[T](req: HttpRequest[T]) = Some((req match {
     case XForwardedFor(forwarded,  _) =>
-      forwarded.filterNot(TrustedProxies.findFirstMatchIn(_).isDefined) match {
+      forwarded.filter(!TrustedProxies.findFirstMatchIn(_).isDefined) match {
         case addr :: _ => addr
         case _ => req.remoteAddr
       }
