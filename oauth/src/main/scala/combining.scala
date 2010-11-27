8fa9e7f58a3b631f8904a9abb4c29d8a3886026c
Nathan Hamblen <nathan@technically.us>
2010-11-27 16:01:53 -0500

    touching up

--- oauth/src/main/scala/combining.scala
+++ oauth/src/main/scala/combining.scala
@@ -1,6 +1,8 @@
 package unfiltered.oauth
 
 trait Combining { this: Encoding =>
-  def combine(strs: String*) = strs map { encode } mkString("&")
-  def combine(parts: Map[String, String]) = parts map { case (k, v) => "%s=%s" format(encode(k), encode(v)) } mkString "&"
+  def combine(strs: String*) = strs.map(encode).mkString("&")
+  def combine(parts: Map[String, String]) = parts map { 
+    case (k, v) => (k :: v :: Nil).map(encode).mkString("=")
+  } mkString "&"
 }
