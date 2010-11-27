c119aa016c6f06f7abffe8208cd5ea6d02c0ee8f
softprops <d.tangren@gmail.com>
2010-11-27 01:18:42 -0500

    utf-8 templates

--- oauth/src/main/scala/templates.scala
+++ oauth/src/main/scala/templates.scala
@@ -5,6 +5,7 @@ import unfiltered.response.Html
 trait OAuthTemplates {
   def layout(body: xml.NodeSeq) = Html(
     <html>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
       <head>
         <title>oauth template</title>
       </head>
