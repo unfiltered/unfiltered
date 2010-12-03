eb9710be3ef2903b1db2fbf9d478fd5646938c27
Nathan Hamblen <nathan@technically.us>
2010-12-02 20:08:35 -0500

    use basic no-charset for ContentType

--- library/src/main/scala/response/types.scala
+++ library/src/main/scala/response/types.scala
@@ -7,18 +7,18 @@ trait BaseContentType extends Responder[Any] {
   def contentType: String
 }
 
-case class ContentType(content_type: String) extends BaseContentType {
+case class CharContentType(content_type: String) extends BaseContentType {
   def contentType = "%s; charset=%s".format(content_type, charset)
   def charset = "utf-8"
 }
-object CssContent extends ContentType("text/css")
-object HtmlContent extends ContentType("text/html")
-object JsContent extends ContentType("text/javascript")
-object CsvContent extends ContentType("text/csv")
-object TextXmlContent extends ContentType("text/xml")
-object PlainTextContent extends ContentType("text/plain")
-object JsonContent extends ContentType("application/json")
-object ApplicationXmlContent extends ContentType("application/xml")
+object CssContent extends CharContentType("text/css")
+object HtmlContent extends CharContentType("text/html")
+object JsContent extends CharContentType("text/javascript")
+object CsvContent extends CharContentType("text/csv")
+object TextXmlContent extends CharContentType("text/xml")
+object PlainTextContent extends CharContentType("text/plain")
+object JsonContent extends CharContentType("application/json")
+object ApplicationXmlContent extends CharContentType("application/xml")
 
-case class BinaryContentType(override val contentType: String) extends BaseContentType
-object PdfContent extends BinaryContentType("application/pdf")
+case class ContentType(override val contentType: String) extends BaseContentType
+object PdfContent extends ContentType("application/pdf")
