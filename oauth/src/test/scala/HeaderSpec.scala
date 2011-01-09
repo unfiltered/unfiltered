106157aa0c29e3805637b8972086b7c537caaa3b
Nathan Hamblen <nathan@technically.us>
2011-01-09 17:26:36 -0500

    compile fix for 2.7.7 triple-quoted str bug

--- oauth/src/test/scala/HeaderSpec.scala
+++ oauth/src/test/scala/HeaderSpec.scala
@@ -7,18 +7,18 @@ object HeaderSpec extends Specification {
 
   "An oAuth Header" should {
     "extract only expected protocol from OAuth Authorization header values" in {
-      val values = """OAuth realm="Example"""" ::
-        """oauth_consumer_key="0685bd9184jfhq22"""" ::
-        """oauth_consumer_key:"malformed"""" ::
-        """oauth_token="ad180jjd733klru7"""" ::
-        """oauth_signature_method="HMAC-SHA1"""" ::
-        """oauth_signature="wOJIO9A2W5mFwDgiDvZbTSMK%2FPY%3D"""" ::
-        """oauth_timestamp="137131200"""" ::
-        """oauth_nonce="4572616e48616d6d65724c61686176"""" ::
-        """oauth_callback="oob"""" ::
-        """oauth_verifier="asdfasdfasd"""" ::
-        """oauth_version="1.0"""" ::
-        """non_protocol_param="bogus"""":: Nil
+      val values = "OAuth realm=\"Example\"" ::
+        "oauth_consumer_key=\"0685bd9184jfhq22\"" ::
+        "oauth_consumer_key:\"malformed\"" ::
+        "oauth_token=\"ad180jjd733klru7\"" ::
+        "oauth_signature_method=\"HMAC-SHA1\"" ::
+        "oauth_signature=\"wOJIO9A2W5mFwDgiDvZbTSMK%2FPY%3D\"" ::
+        "oauth_timestamp=\"137131200\"" ::
+        "oauth_nonce=\"4572616e48616d6d65724c61686176\"" ::
+        "oauth_callback=\"oob\"" ::
+        "oauth_verifier=\"asdfasdfasd\"" ::
+        "oauth_version=\"1.0\"" ::
+        "non_protocol_param=\"bogus\"":: Nil
       val extractedOpt = OAuth.Header.unapply(values)
       extractedOpt must beSomething
       val extracted = extractedOpt.get
