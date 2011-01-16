07de1c2de54e039db9b58f3f0644eb6c23d5e988
doug <d.tangren@gmail.com>
2011-01-15 20:17:09 -0500

    multiple handlers need unique identifiers. closes gh-5

--- netty-server/src/main/scala/servers.scala
+++ netty-server/src/main/scala/servers.scala
@@ -9,6 +9,7 @@ import org.jboss.netty.handler.codec.http.{HttpRequestDecoder, HttpResponseEncod
 import org.jboss.netty.channel._
 import group.{ChannelGroup, DefaultChannelGroup}
 import unfiltered._
+import java.util.concurrent.atomic.AtomicInteger
 
 /** Default implementation of the Server trait. If you want to use a custom pipeline
  * factory it's better to extend Server directly. */
@@ -16,20 +17,20 @@ case class Http(port: Int, host: String,
                 handlers: List[ChannelHandler],
                 beforeStopBlock: () => Unit) extends Server with RunnableServer {
   def pipelineFactory: ChannelPipelineFactory = new ServerPipelineFactory(channels, handlers)
-  
+
   def stop() = {
     beforeStopBlock()
     closeConnections()
     destroy()
   }
-  def handler(h: ChannelHandler) = 
+  def handler(h: ChannelHandler) =
     Http(port, host, h :: handlers, beforeStopBlock)
   def beforeStop(block: => Unit) =
     Http(port, host, handlers, { () => beforeStopBlock(); block })
 }
 
 object Http {
-  def apply(port: Int, host: String): Http = 
+  def apply(port: Int, host: String): Http =
     Http(port, host, Nil, () => ())
   def apply(port: Int): Http =
     Http(port, "0.0.0.0")
@@ -42,9 +43,9 @@ trait Server extends RunnableServer {
 
   val DEFAULT_IO_THREADS = Runtime.getRuntime().availableProcessors() + 1;
   val DEFAULT_EVENT_THREADS = DEFAULT_IO_THREADS * 4;
-  
+
   private var bootstrap: ServerBootstrap = _
-  
+
   /** any channels added to this will receive broadcasted events */
   protected val channels = new DefaultChannelGroup("Netty Unfiltered Server Channel Group")
 
@@ -64,7 +65,7 @@ trait Server extends RunnableServer {
     channels.add(bootstrap.bind(new InetSocketAddress(host, port)))
     this
   }
-  
+
   def closeConnections(): this.type = {
     // Close any pending connections / channels (including server)
     channels.close.awaitUninterruptibly
@@ -77,8 +78,8 @@ trait Server extends RunnableServer {
   }
 }
 
-class ServerPipelineFactory(val channels: ChannelGroup, 
-                            val handlers: List[ChannelHandler]) 
+class ServerPipelineFactory(val channels: ChannelGroup,
+                            val handlers: List[ChannelHandler])
     extends ChannelPipelineFactory with DefaultPipelineFactory {
   def getPipeline(): ChannelPipeline = complete(Channels.pipeline)
 }
@@ -87,10 +88,13 @@ trait DefaultPipelineFactory {
   def channels: ChannelGroup
   def handlers: List[ChannelHandler]
   protected def complete(line: ChannelPipeline) = {
+    val counter = new AtomicInteger
     line.addLast("housekeeping", new HouseKeepingChannelHandler(channels))
     line.addLast("decoder", new HttpRequestDecoder)
     line.addLast("encoder", new HttpResponseEncoder)
-    handlers.reverse.foreach { h => line.addLast("handler", h) }
+    handlers.reverse.foreach {
+      line.addLast("handler-%s" format counter.incrementAndGet, _)
+    }
     line
   }
 }
