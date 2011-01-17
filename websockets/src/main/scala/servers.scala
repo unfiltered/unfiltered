82121b1e77e05868e9088a78da6f2e0da27e5905
doug <d.tangren@gmail.com>
2011-01-16 22:12:11 -0500

    pass a 'pass' interface for websockets

--- websockets/src/main/scala/servers.scala
+++ websockets/src/main/scala/servers.scala
@@ -9,29 +9,30 @@ object WebSocketServer {
       new WebSocketServer(port, host, path, intent).run
 }
 
-case class WebSocketServer(port: Int, host: String, path: String, 
-                           intent: PartialFunction[SocketCallback, Unit]) 
+case class WebSocketServer(port: Int, host: String, path: String,
+                           intent: PartialFunction[SocketCallback, Unit])
     extends NServer with unfiltered.util.RunnableServer {
   import org.jboss.{netty => netty}
   import netty.channel.{ChannelPipeline, ChannelPipelineFactory, ChannelStateEvent}
   import netty.handler.codec.http.{HttpChunkAggregator, HttpRequestDecoder, HttpResponseEncoder}
   import netty.channel.Channels._
-  
+
   def stop() = {
     closeConnections()
     destroy()
   }
-  def this(port: Int, path: String, intent: PartialFunction[SocketCallback, Unit]) = 
+
+  def this(port: Int, path: String, intent: PartialFunction[SocketCallback, Unit]) =
     this(port, "0.0.0.0", path, intent)
-  
+
   lazy val pipelineFactory = new ChannelPipelineFactory {
     def getPipeline = (pipeline /: pipings) ((p, e) => { p.addLast(e._1, e._2); p })
     lazy val pipings = Map(
       "decoder" -> new HttpRequestDecoder,
       "aggregator" -> new HttpChunkAggregator(65536),
       "encoder" -> new HttpResponseEncoder,
-      "handler" -> new WebSocketHandler(path, intent),
+      "handler" -> WebSocketHandler(path, intent),
       "housekeeping" -> new HouseKeepingChannelHandler(channels)
-    ) 
+    )
   }
 }
--- websockets/src/main/scala/websockets.scala
+++ websockets/src/main/scala/websockets.scala
@@ -1,9 +1,9 @@
 package unfiltered.websockets
 
-import org.jboss.{netty => netty}
+import org.jboss.{netty => jnetty}
 
-import netty.channel.{Channel, SimpleChannelUpstreamHandler}
-import netty.buffer.ChannelBuffer
+import jnetty.channel.{Channel, ChannelHandlerContext, MessageEvent, SimpleChannelUpstreamHandler}
+import jnetty.buffer.ChannelBuffer
 
 trait SocketCallback
 case class Open(socket: WebSocket) extends SocketCallback
@@ -13,129 +13,138 @@ case class Error(socket: WebSocket, err: Throwable) extends SocketCallback
 
 sealed trait Msg
 case class Text(txt: String) extends Msg
-case class Binary(buf: netty.buffer.ChannelBuffer) extends Msg
+case class Binary(buf: jnetty.buffer.ChannelBuffer) extends Msg
 
-case class WebSocket(c: Channel) {
-  import netty.handler.codec.http.websocket.DefaultWebSocketFrame
+case class WebSocket(channel: Channel) {
+  import jnetty.handler.codec.http.websocket.DefaultWebSocketFrame
 
-  def send(str: String) = c.write(new DefaultWebSocketFrame(str))
+  def send(str: String) = channel.write(new DefaultWebSocketFrame(str))
 
   /** will throw an IllegalArgumentException if (type & 0x80 == 0) and the data is not
    * encoded in UTF-8 */
-  def send(mtype: Int, buf: ChannelBuffer) = c.write(new DefaultWebSocketFrame(mtype, buf))
+  def send(mtype: Int, buf: ChannelBuffer) = channel.write(new DefaultWebSocketFrame(mtype, buf))
 }
 
-class WebSocketHandler(path: String, intent: PartialFunction[SocketCallback, Unit])
+object WebSocketHandler {
+  import jnetty.channel.ChannelFutureListener
+  import jnetty.buffer.ChannelBuffers
+  import jnetty.handler.codec.http.{HttpRequest => NHttpRequest, DefaultHttpResponse}
+  import jnetty.handler.codec.http.HttpVersion.HTTP_1_1
+  import jnetty.handler.codec.http.HttpResponseStatus.FORBIDDEN
+  import jnetty.handler.codec.http.HttpHeaders._
+  import jnetty.util.CharsetUtil
+
+  type Intent = PartialFunction[SocketCallback, Unit]
+
+  /** Creates a WebSocketHandler that, when passes, will return a forbidden
+   *  response to the client */
+  def apply(path: String, intent: Intent): WebSocketHandler =
+    WebSocketHandler(path, intent, { (ctx, event) =>
+      event.getMessage match {
+        case request: NHttpRequest =>
+          val res = new DefaultHttpResponse(HTTP_1_1, FORBIDDEN)
+          res.setContent(ChannelBuffers.copiedBuffer(res.getStatus.toString, CharsetUtil.UTF_8))
+          setContentLength(res, res.getContent.readableBytes)
+          ctx.getChannel.write(res).addListener(ChannelFutureListener.CLOSE)
+        case msg => error("Invalid pass message type (%s) for WebSocketHandler" format msg.getClass.getName)
+      }
+    })
+}
+
+case class WebSocketHandler(path: String, intent: WebSocketHandler.Intent,
+                            pass: (ChannelHandlerContext, MessageEvent) => Unit)
   extends SimpleChannelUpstreamHandler {
 
   import java.security.MessageDigest
-  import netty.channel.{ChannelFuture, ChannelFutureListener, ChannelHandlerContext,
-                        ChannelStateEvent, ExceptionEvent, MessageEvent}
-  import netty.buffer.ChannelBuffers
-  import netty.handler.codec.http.websocket.{DefaultWebSocketFrame, WebSocketFrame,
+  import jnetty.channel.{ChannelFuture, ChannelFutureListener,
+                         ChannelStateEvent, ExceptionEvent}
+  import jnetty.buffer.ChannelBuffers
+  import jnetty.handler.codec.http.websocket.{DefaultWebSocketFrame, WebSocketFrame,
                                              WebSocketFrameDecoder, WebSocketFrameEncoder}
-  import netty.handler.codec.http.{HttpHeaders, HttpMethod, HttpRequest => NHttpRequest,
-                                   HttpResponseStatus, HttpVersion, DefaultHttpResponse,
+  import jnetty.handler.codec.http.{HttpHeaders, HttpMethod, HttpRequest => NHttpRequest,
+                                   HttpResponseStatus, HttpVersion,  DefaultHttpResponse,
                                    HttpResponse => NHttpResponse}
-  import netty.handler.codec.http
+  import jnetty.handler.codec.http
   import HttpHeaders._
-  import HttpHeaders.Names._
+  import HttpHeaders.Names.{CONNECTION, ORIGIN, HOST, UPGRADE, SEC_WEBSOCKET_LOCATION,
+                            SEC_WEBSOCKET_ORIGIN, SEC_WEBSOCKET_PROTOCOL,
+                            SEC_WEBSOCKET_KEY1, SEC_WEBSOCKET_KEY2}
   import HttpHeaders.Values._
-  import HttpMethod._
-  import HttpResponseStatus._
-  import HttpVersion._
-  import netty.util.CharsetUtil
+
+  import jnetty.util.CharsetUtil
 
   /** 0x00-0x7F typed frame becomes (UTF-8) Text
       0x80-0xFF typed frame becomes Binary */
   implicit def wsf2msg(wsf: WebSocketFrame): Msg =
     if(wsf.isText) Text(wsf.getTextData) else Binary(wsf.getBinaryData)
 
-  /** attempt to handle the intent (moving towards unfiltered interface) */
-  def attempt(request: SocketCallback) =
-    (try {
-      intent(request)
-    } catch {
-      case m: MatchError => ()
-    })
+  def attempt = intent.orElse({ case _ => () }: WebSocketHandler.Intent)
 
-  override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent) = e.getMessage match {
-    case http: NHttpRequest => handshake(ctx, http)
-    case ws: WebSocketFrame => frame(ctx, ws)
-  }
-
-  override def exceptionCaught(ctx: ChannelHandlerContext, e: ExceptionEvent) = {
-    attempt(Error(WebSocket(ctx.getChannel), e.getCause))
-    e.getChannel.close
-  }
-
-  private def handshake(ctx: ChannelHandlerContext, req: NHttpRequest) = req.getMethod match {
-    case GET => req.getUri match {
-      case p: String if(p.equals(path)) =>
-        if (Values.UPGRADE.equalsIgnoreCase(req.getHeader(CONNECTION)) &&
-            WEBSOCKET.equalsIgnoreCase(req.getHeader(Names.UPGRADE))) {
-          val res = new DefaultHttpResponse(HTTP_1_1,
-                                            new HttpResponseStatus(101, "Web Socket Protocol Handshake"))
-
-          def head(k: String, v: String) = res.addHeader(k, v)
-          def headIfDefined(k: String) = if(req.getHeader(k) != null) head(k, req.getHeader(k))
-
-          head(Names.UPGRADE, WEBSOCKET)
-          head(CONNECTION, Values.UPGRADE)
-          head(SEC_WEBSOCKET_ORIGIN, req.getHeader(ORIGIN))
-          head(SEC_WEBSOCKET_LOCATION, location(req))
-          headIfDefined(SEC_WEBSOCKET_PROTOCOL)
-
-          if (req.containsHeader(SEC_WEBSOCKET_KEY1) && req.containsHeader(SEC_WEBSOCKET_KEY2)) {
-            val in = ChannelBuffers.buffer(16)
-            List(req.getHeader(SEC_WEBSOCKET_KEY1), req.getHeader(SEC_WEBSOCKET_KEY2)).foreach( k =>
-              in.writeInt((k.replaceAll("[^0-9]", "").toLong / k.replaceAll("[^ ]", "").length).toInt)
-            )
-            in.writeLong(req.getContent().readLong)
-            res.setContent(ChannelBuffers.wrappedBuffer(MessageDigest.getInstance("MD5").digest(in.array)))
-          }
-
-          val p = ctx.getChannel.getPipeline
-          p.get("aggregator") match {
-            case null => ()
-            case _ => p.remove("aggregator")
-          }
-          p.replace("decoder","wsdecoder", new WebSocketFrameDecoder)
-          ctx.getChannel.write(res)
-          ctx.getChannel.getCloseFuture.addListener(new ChannelFutureListener {
-            def operationComplete(future: ChannelFuture) =
-             attempt(Close(WebSocket(ctx.getChannel)))
-          })
-          p.replace("encoder", "wsencoder", new WebSocketFrameEncoder)
-
-          attempt(Open(WebSocket(ctx.getChannel)))
-
-        } else {
-          forbid(ctx, req)
-        }
-      /* some other path */
-      case _ => forbid(ctx, req)
+  override def messageReceived(ctx: ChannelHandlerContext, event: MessageEvent) =
+    event.getMessage match {
+      case http: NHttpRequest => upgrade(ctx, http, event)
+      case frame: WebSocketFrame => attempt(Message(WebSocket(ctx.getChannel), frame))
     }
-    /* some other http method */
-    case _ => forbid(ctx, req)
+
+  override def exceptionCaught(ctx: ChannelHandlerContext, event: ExceptionEvent) = {
+    attempt(Error(WebSocket(ctx.getChannel), event.getCause))
+    event.getChannel.close
   }
 
-  private def frame(ctx: ChannelHandlerContext, frame: WebSocketFrame) =
-    attempt(Message(WebSocket(ctx.getChannel), frame))
+  private def upgrade(ctx: ChannelHandlerContext, request: NHttpRequest, event: MessageEvent) =
+    request.getMethod match {
+      case HttpMethod.GET => request.getUri match {
+        case p: String if(p.equals(path)) =>
+          if (Values.UPGRADE.equalsIgnoreCase(request.getHeader(CONNECTION)) &&
+              Values.WEBSOCKET.equalsIgnoreCase(request.getHeader(UPGRADE))) {
 
-  private def forbid(ctx: ChannelHandlerContext, req: NHttpRequest) =
-    sendHttpResponse(ctx, req, new DefaultHttpResponse(HTTP_1_1, FORBIDDEN))
+            val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, new HttpResponseStatus(101, "Web Socket Protocol Handshake"))
 
-  private def sendHttpResponse(ctx: ChannelHandlerContext, req: NHttpRequest, res: NHttpResponse) = {
-    if(res.getStatus.getCode != 200) {
-      res.setContent(ChannelBuffers.copiedBuffer(res.getStatus.toString, CharsetUtil.UTF_8))
-      setContentLength(res, res.getContent.readableBytes)
+            def head(k: String, v: String) = response.addHeader(k, v)
+            def headIfDefined(k: String) = if(request.getHeader(k) != null) head(k, request.getHeader(k))
+
+            head(UPGRADE, Values.WEBSOCKET)
+            head(CONNECTION, Values.UPGRADE)
+            head(SEC_WEBSOCKET_ORIGIN, request.getHeader(ORIGIN))
+            head(SEC_WEBSOCKET_LOCATION, location(request))
+            headIfDefined(SEC_WEBSOCKET_PROTOCOL)
+
+            if (request.containsHeader(SEC_WEBSOCKET_KEY1) && request.containsHeader(SEC_WEBSOCKET_KEY2)) {
+              val buff = ChannelBuffers.buffer(16)
+              List(request.getHeader(SEC_WEBSOCKET_KEY1), request.getHeader(SEC_WEBSOCKET_KEY2)).foreach( k =>
+                buff.writeInt((k.replaceAll("[^0-9]", "").toLong / k.replaceAll("[^ ]", "").length).toInt)
+              )
+              buff.writeLong(request.getContent().readLong)
+              response.setContent(ChannelBuffers.wrappedBuffer(MessageDigest.getInstance("MD5").digest(buff.array)))
+            }
+
+            val pipe = ctx.getChannel.getPipeline
+            if(pipe.get("aggregator") != null) {
+              pipe.remove("aggregator")
+            }
+            pipe.replace("decoder", "wsdecoder", new WebSocketFrameDecoder)
+
+            ctx.getChannel.write(response)
+            ctx.getChannel.getCloseFuture.addListener(new ChannelFutureListener {
+              def operationComplete(future: ChannelFuture) =
+               attempt(Close(WebSocket(ctx.getChannel)))
+            })
+
+            pipe.replace("encoder", "wsencoder", new WebSocketFrameEncoder)
+
+            attempt(Open(WebSocket(ctx.getChannel)))
+
+          } else pass(ctx, event) /* not a handshake request */
+
+        case _ => pass(ctx, event) /* some other path */
+      }
+      case _ => pass(ctx, event) /* some other http method */
     }
-    val f = ctx.getChannel.write(res)
-    if(!isKeepAlive(req) || res.getStatus.getCode != 200) {
-      f.addListener(ChannelFutureListener.CLOSE)
-    }
-  }
 
-  private def location(req: NHttpRequest) = "ws://%s%s" format(req.getHeader(HttpHeaders.Names.HOST), path)
+  /** By default, when a websocket handler `passes` to writes an Http Forbidden response
+   *  to the channel. To override that behavior. call this method with a function to handle
+   *  the MessageEvent with custom behavior */
+  def onPass(handler: (ChannelHandlerContext, MessageEvent) => Unit) = WebSocketHandler(path, intent, handler)
+
+  private def location(req: NHttpRequest) = "ws://%s%s" format(req.getHeader(HOST), path)
 }
