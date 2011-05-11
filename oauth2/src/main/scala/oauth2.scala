30daaa6cb24736077ab7b56c449ae2cdba106639
softprops <d.tangren@gmail.com>
2011-05-10 22:51:42 -0400

    provide an extactor for injected resource owner for upstream filters

--- oauth2/src/main/scala/oauth2.scala
+++ oauth2/src/main/scala/oauth2.scala
@@ -4,3 +4,19 @@ object OAuth2 {
   val XAuthorizedIdentity = "X-Authorized-Identity"
   val XAuthorizedScopes = "X-Authorized-Scopes"
 }
+
+/** resource owner extractor */
+object OAuthResourceOwner {
+  import OAuth2._
+  import javax.servlet.http.HttpServletRequest
+  import unfiltered.request.HttpRequest
+
+  def unapply[T <: HttpServletRequest](r: HttpRequest[T]): Option[(String, Option[String])] =
+    r.underlying.getAttribute(XAuthorizedIdentity) match {
+      case null => None
+      case id: String => r.underlying.getAttribute(XAuthorizedScopes) match {
+         case null => Some((id, None))
+         case scopes: String => Some((id, Some(scopes)))
+      }
+    }
+}
