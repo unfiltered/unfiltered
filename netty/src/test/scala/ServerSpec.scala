72b6b2f4796f3c12b7ccb824344dceb81f8dea12
Nathan Hamblen <nathan@technically.us>
2011-05-17 17:38:37 -0400

    handle Pass in netty channel Plan, change intent return type to Any, add specs

--- netty/src/test/scala/ServerSpec.scala
+++ netty/src/test/scala/ServerSpec.scala
@@ -13,10 +13,14 @@ object ServerSpec extends unfiltered.spec.netty.Served {
     case GET(UFPath("/")) => ResponseString("test") ~> Ok
     case r @ GET(UFPath("/addr")) => ResponseString(r.remoteAddr) ~> Ok
     case GET(UFPath("/addr_extractor") & RemoteAddr(addr)) => ResponseString(addr) ~> Ok
-  })).handler(cycle.Planify {
+  })).handler(channel.Planify({
+    case GET(UFPath("/pass")) => Pass
+    case req @ GET(UFPath("/planc")) =>
+      req.underlying.respond(ResponseString("planc") ~> Ok)
+  })).handler(cycle.Planify({
     case GET(UFPath("/planb")) => ResponseString("planb") ~> Ok
     case GET(UFPath("/pass")) => ResponseString("pass") ~> Ok
-  })
+  }))
   
   "A Server" should {
     "respond to requests" in {
@@ -31,10 +35,13 @@ object ServerSpec extends unfiltered.spec.netty.Served {
     "provide a remote address accounting for X-Forwared-For header filtering private addresses" in {
       Http(host / "addr_extractor" <:< Map("X-Forwarded-For" -> "172.31.255.255") as_str) must_=="127.0.0.1"
     }
-    "respond to requests in second channel handler" in {
+    "respond to requests in ustream channel plan" in {
+      Http(host / "planc" as_str) must_=="planc"
+    }
+    "respond to requests in last channel handler" in {
       Http(host / "planb" as_str) must_=="planb"
     }
-    "pass upstream on Pass, respond in second handler" in {
+    "pass upstream on Pass, respond in last handler" in {
       Http(host / "pass" as_str) must_=="pass"
     }
   }
