7757896afa1a1091777c32b6a2996341a5635ae4
Nathan Hamblen <nathan@technically.us>
2011-05-17 23:42:57 -0400

    pamflet 0.1.2

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -3,5 +3,5 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val sxr_publish = "net.databinder" % "sxr-publish" % "0.2.0"
   val posterous = "net.databinder" % "posterous-sbt" % "0.1.7"
-  val pf = "net.databinder" % "pamflet-plugin" % "0.1.2-SNAPSHOT"
+  val pf = "net.databinder" % "pamflet-plugin" % "0.1.2"
 }
