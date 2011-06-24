0a7e7e4690fa38a91aae47792a6cdebf6574ce0f
softprops <d.tangren@gmail.com>
2011-06-24 01:46:08 -0400

    actually use the cross scala versions

--- project/build.scala
+++ project/build.scala
@@ -2,29 +2,28 @@ import sbt._
 import Keys._
 
 object Shared {
-  val buildScalaVersion = "2.8.1"
   val buildVersion = "0.4.0-SNAPSHOT"
 
   val servletApiDep = "javax.servlet" % "servlet-api" % "2.3" % "provided"
   val jettyVersion = "7.2.2.v20101205"
 
-  def specsDep =
-    buildScalaVersion.substring(0,3) match {
+  def specsDep(sv: String) =
+    sv.substring(0,3) match {
       case "2.8" => "org.scala-tools.testing" % "specs_2.8.1" % "1.6.8"
       case "2.9" => "org.scala-tools.testing" %% "specs" % "1.6.8"
     }
 
-  def dispatchDep = if(buildScalaVersion startsWith "2.8.1")
+  def dispatchDep(sv: String) = if(sv startsWith "2.8.1")
       "net.databinder" % "dispatch-mime_2.8.0" % "0.7.8"
     else
       "net.databinder" %% "dispatch-mime" % "0.7.8"
 
-  def dispatchOAuthDep = if(buildScalaVersion startsWith "2.8.1")
+  def dispatchOAuthDep(sv: String) = if(sv startsWith "2.8.1")
       "net.databinder" % "dispatch-oauth_2.8.0" % "0.7.8"
     else
       "net.databinder" %% "dispatch-oauth" % "0.7.8"
 
-  def integrationTestDeps = Seq(Shared.specsDep % "test", Shared.dispatchDep % "test")
+  def integrationTestDeps(sv: String) = Seq(Shared.specsDep(sv) % "test", Shared.dispatchDep(sv) % "test")
 }
 
 object Shell {
@@ -72,28 +71,28 @@ object Unfiltered extends Build {
     Project("library", file("library"),
             settings = buildSettings ++ Seq(
               name := "Unfiltered",
-              libraryDependencies := Seq(
+              libraryDependencies <++= scalaVersion( v => Seq(
                 "commons-codec" % "commons-codec" % "1.4",
-                Shared.specsDep % "test"
-             ) ++ integrationTestDeps
+                Shared.specsDep(v) % "test"
+             ) ++ integrationTestDeps(v))
             )) dependsOn(util)
 
   lazy val filters =
     Project("filter", file("filter"),
           settings = buildSettings ++ Seq(
             name := "Unfiltered Filter",
-            libraryDependencies := Seq(servletApiDep) ++ integrationTestDeps
+            libraryDependencies <++= scalaVersion( v => Seq(servletApiDep) ++ integrationTestDeps(v))
           )) dependsOn(library)
 
   lazy val uploads =
     Project("uploads", file("uploads"),
           settings = buildSettings ++ Seq(
             name := "Unfiltered Uploads",
-            libraryDependencies := Seq(
+            libraryDependencies <++= scalaVersion(Seq(
               servletApiDep,
               "commons-io" % "commons-io" % "1.4",
               "commons-fileupload" % "commons-fileupload" % "1.2.1"
-            ) ++ integrationTestDeps)) dependsOn(filters)
+            ) ++ integrationTestDeps _))) dependsOn(filters)
 
   lazy val util =
     Project("util", file("util"),
@@ -128,30 +127,30 @@ object Unfiltered extends Build {
     Project("netty", file("netty"),
             settings = buildSettings ++ Seq(
               name := "Unfiltered Netty",
-              libraryDependencies := integrationTestDeps
+              libraryDependencies <++= scalaVersion(integrationTestDeps _)
             )) dependsOn(nettyServer, library)
 
   lazy val specHelpers =
     Project("spec", file("spec"),
             settings = buildSettings ++ Seq(
               name := "Unfiltered Spec",
-              libraryDependencies := Seq(specsDep, dispatchDep)
+              libraryDependencies <++= scalaVersion(v => Seq(specsDep(v), dispatchDep(v)))
             )) dependsOn(jetty, netty)
 
   lazy val scalaTestHelpers =
     Project("scalatest", file("scalatest"),
           settings = buildSettings ++ Seq(
             name := "Unfiltered Scalatest",
-            libraryDependencies := Seq("org.scalatest" % "scalatest" % "1.3", dispatchDep)
+            libraryDependencies <++= scalaVersion(v => Seq("org.scalatest" % "scalatest" % "1.3", dispatchDep(v)))
           )) dependsOn(jetty, netty)
 
   lazy val json =
     Project("json", file("json"),
           settings = buildSettings ++ Seq(
             name := "Unfiltered Json",
-            libraryDependencies := Seq(
-              if (buildScalaVersion.startsWith("2.8")) "net.liftweb" %% "lift-json" % "2.3"
-              else "net.liftweb" %% "lift-json" % "2.4-M2") ++ integrationTestDeps
+            libraryDependencies <++= scalaVersion(v => Seq(
+              if (v.startsWith("2.8")) "net.liftweb" %% "lift-json" % "2.3"
+              else "net.liftweb" %% "lift-json" % "2.4-M2") ++ integrationTestDeps(v))
           )) dependsOn(library)
 
   lazy val scalate =
@@ -159,24 +158,24 @@ object Unfiltered extends Build {
             settings = buildSettings ++ Seq(
               name := "Unfiltered Scalate",
               resolvers += ScalaToolsSnapshots,
-              libraryDependencies := Seq(
+              libraryDependencies <++= scalaVersion(v => Seq(
                 "org.fusesource.scalate" % "scalate-core" % "1.4.1",
                 "org.fusesource.scalate" % "scalate-util" % "1.4.1" % "test",
-                "org.scala-lang" % "scala-compiler" % buildScalaVersion % "test",
+                "org.scala-lang" % "scala-compiler" % v % "test",
                 "org.mockito" % "mockito-core" % "1.8.5" % "test"
-              ) ++ integrationTestDeps)) dependsOn(library)
+              ) ++ integrationTestDeps(v)))) dependsOn(library)
 
   lazy val websockets =
     Project("websockets", file("websockets"),
           settings = buildSettings ++ Seq(
             name := "Unfiltered Websockets",
-            libraryDependencies := integrationTestDeps
+            libraryDependencies <++= scalaVersion(integrationTestDeps _)
           )) dependsOn(netty)
 
   lazy val oauth =
     Project("oauth", file("oauth"),
           settings = buildSettings ++ Seq(
             name := "Unfiltered OAuth",
-            libraryDependencies := Seq(dispatchOAuthDep) ++ integrationTestDeps
+            libraryDependencies <++= scalaVersion(v => Seq(dispatchOAuthDep(v)) ++ integrationTestDeps(v))
           )) dependsOn(jetty, filters)
 }
