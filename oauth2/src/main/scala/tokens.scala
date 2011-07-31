3441905b6ae6ab188f357066ebad404c5e30d474
Timothy Perrett <timothy@getintheloop.eu>
2011-07-31 14:42:51 +0100

    More docs

--- oauth2/src/main/scala/tokens.scala
+++ oauth2/src/main/scala/tokens.scala
@@ -34,21 +34,65 @@ trait Token {
  * with the some kind of storage
  */
 trait TokenStore {
-  /** @return a refreshed or new token given a
-   * valid access token */
+  /** 
+   * 
+   * @see AuthorizationServer
+   * @see RefreshTokenRequest
+   * @return Gives a refreshed or new token given a valid access token
+   */
   def refresh(other: Token): Token
-  def token(code: String): Option[Token]
-  /** query for Token by client */
+
+  /** 
+   * 
+   * query for Token by client 
+   * @see AuthorizationServer
+   * @see RefreshTokenRequest
+   * @return Given the a refresh token gives a new access token
+   */
   def refreshToken(refreshToken: String): Option[Token]
-  /** create an access token given a code token */
+  
+  /**
+   * 
+   * @see AuthorizationServer
+   * @see AccessTokenRequest
+   * @return Given a "code" return a resource access token
+   */
+  def token(code: String): Option[Token]
+
+  /** 
+   * 
+   * @see AuthorizationServer
+   * @see AccessTokenRequest
+   * @return Create an access token given a code token 
+   */
   def exchangeAuthorizationCode(codeToken: Token): Token
-  /** @return an access token for a given client, not tied to a given resource owner */
-  def generateClientToken(client: Client, scope: Option[String]): Token
-  /** @return a short lived authorization code bound to a client
-   *  and redirect uri for a given resource owner. */
-  def generateAuthorizationCode(owner: ResourceOwner, client: Client,
-                        scope: Option[String], redirectUri: String): String
-  /** @return a access token for an implicit client */
+  
+  /** 
+   * 
+   * @see AuthorizationServer
+   * @see AuthorizationCodeRequest
+   * @return a short lived authorization code bound to a client
+   * and redirect uri for a given resource owner. 
+   */
+  def generateAuthorizationCode(
+        owner: ResourceOwner, client: Client,
+        scope: Option[String], redirectUri: String): String
+  
+  /** 
+   * 
+   * @see AuthorizationServer
+   * @see ImplicitAuthorizationRequest
+   * @return an access token for an implicit client
+   */
   def generateImplicitAccessToken(owner: ResourceOwner, client: Client,
                                   scope: Option[String], redirectUri: String): Token
+
+  /** 
+   * 
+   * @see AuthorizationServer
+   * @see ClientCredentialsRequest
+   * @return an access token for a given client, not tied to a 
+   * given resource owner
+   */
+  def generateClientToken(client: Client, scope: Option[String]): Token
 }
