e43fdbbbbb16f55b69f10c47a640dcc146140a33
Nathan Hamblen <nathan@technically.us>
2011-07-31 20:35:32 -0400

    passing scalate tests

--- scalate/src/main/scala/scalate.scala
+++ scalate/src/main/scala/scalate.scala
@@ -3,8 +3,10 @@ package unfiltered.scalate
 
 import org.fusesource.scalate.{TemplateEngine, Binding, DefaultRenderContext, RenderContext}
 
-import unfiltered.response.{HttpResponse, Responder}
+import unfiltered.response.{ResponseWriter}
 import unfiltered.request.HttpRequest
+
+import java.io.Writer
 
 /**
  * private object that holds the default engine
@@ -13,15 +15,15 @@ private[scalate] object ScalateDefaults{
   val defaultTemplateDirs = List(new java.io.File("src/main/resources/templates"))
   implicit val engine = new TemplateEngine(defaultTemplateDirs)
 
-  implicit def renderContext(req: HttpRequest[_], res: HttpResponse[_], engine: TemplateEngine) =
+  implicit def renderContext(req: HttpRequest[_], writer: Writer, engine: TemplateEngine) =
     new DefaultRenderContext(
       unfiltered.util.Optional(req.uri).getOrElse("").split('?')(0),
-      engine, new java.io.PrintWriter(res.getOutputStream))
+      engine, new java.io.PrintWriter(writer))
 }
 
 object Scalate {
   /* An arity-3 function that constructs an appropriate RenderContext. */
-  type RenderContextBuilder[A, B] = (HttpRequest[A], HttpResponse[B], TemplateEngine) => RenderContext
+  type RenderContextBuilder[A, B] = (HttpRequest[A], Writer, TemplateEngine) => RenderContext
 }
 
 /**
@@ -35,17 +37,13 @@ case class Scalate[A, B](request: HttpRequest[A], template: String, attributes:(
     contextBuilder: Scalate.RenderContextBuilder[A, B] = ScalateDefaults.renderContext _,
     bindings: List[Binding] = List[Binding](),
     additionalAttributes: List[(String, Any)] = List[(String, Any)]()
-  ) extends Responder[B]{
+  ) extends ResponseWriter {
 
-  def respond(res: HttpResponse[B]) {
-    val stream = res.getOutputStream()
-    try {
-      val scalateTemplate = engine.load(template, bindings)
-      val context = contextBuilder(request, res, engine)
-      for(attr <- additionalAttributes) context.attributes(attr._1) = attr._2
-      for(attr <- attributes) context.attributes(attr._1) = attr._2
-      engine.layout(scalateTemplate, context)
-    }
-    finally { stream.close() }
+  def write(writer: Writer) {
+    val scalateTemplate = engine.load(template, bindings)
+    val context = contextBuilder(request, writer, engine)
+    for(attr <- additionalAttributes) context.attributes(attr._1) = attr._2
+    for(attr <- attributes) context.attributes(attr._1) = attr._2
+    engine.layout(scalateTemplate, context)
   }
 }
