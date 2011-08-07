1706bedecf9de86d1cd3755c6d69f93b6fa64f8e
Chris Lewis <chris@thegodcode.net>
2011-08-07 15:29:42 -0400

    coverage over ie

--- library/src/main/scala/request/agents.scala
+++ library/src/main/scala/request/agents.scala
@@ -1,49 +1,33 @@
 package unfiltered.request
 
-trait AgentExtractor {
+trait AgentExtractor extends (HttpRequest[_] => Boolean) {
   val test: String => Boolean
   def &(e: AgentExtractor) = new AgentExtractor {
     val test: String => Boolean = (ua) => AgentExtractor.this.test(ua) && e.test(ua)
   }
+  def apply(req: HttpRequest[_]) =
+    UserAgent.unapply(req).map(test).getOrElse(false)
   def unapply[A](req: HttpRequest[A]) =
     UserAgent.unapply(req)
       .flatMap(ua => if(test(ua)) Some(req) else None)
 }
 
 object AgentIs {  
-  
-  /*
-   * User agent strings are an unspecified abyss; anything goes. Wikipedia
-   * states (http://en.wikipedia.org/wiki/User_agent#Format) that many vendors
-   * use a non-standard derivative of RFC1945. Correcting this "spec" with some
-   * real-world examples, regex RFC1945Common should match a large number of
-   * these agents. From the pool of agent strings that should match, but instead
-   * deviate from the pattern in some small way, the subsequent regexes have
-   * been derived. Their names come from the agent that seems to supply the
-   * most strings that match.
-   */
-  // name, version, system, platform, platform_details, extensions
-  val RFC1945Common   = """([\w ]*)/([\d\.]*) ?\((.*)\) (.*) \((.*)\) ?(.*)""".r
-  // name, version, system, platform, extensions
-  val RFC1945FireFox  = """([\w ]*)/([\d\.]*) ?\((.*)\) (\w*[/\d\.]*) ?(.*)""".r
-  // name, version, system
-  val RFC1945IE       = """([\w ]*)/([\d\.]*) \((.*)\)""".r
-  // name, version, os, system, platform, platform_details, extensions
-  val RFC1945Chrome   = """([\w ]*)/([\d\.]*) (.*) \((.*)\) (.*) \((.*)\) ?(.*)""".r
-  
   // An agent matcher is a String => Boolean.
   type AM = String => Boolean
   
   // Matchers for major agents.
   val chromeAM:   AM = _.contains("Chrome")
-  val firefoxAM:  AM = _.toLowerCase.contains("firefox")
+  val firefoxAM:  AM = (ua) => ua.toLowerCase.contains("firefox") && !ua.contains("Opera")
   val operaAM:    AM = _.contains("Opera")
   val ieAM:       AM = (ua) => ua.contains("MSIE") && !ua.contains("Opera")
-  val safariAM:   AM = (ua) => (ua.contains("AppleWebKit") || ua.contains("Safari")) && ! ua.contains("Chrome")
+  val safariAM:   AM = (ua) => (ua.contains("AppleWebKit") || ua.contains("Safari")) && ! (chromeAM(ua) || operaAM(ua))
   // Mobile.
   val mobile:     AM = (ua) => MobileAgent.all.exists(ua.contains(_))
   
-  def agent[A](pf: String => Boolean) = new AgentExtractor { val test = pf }
+  def agent[A](pf: String => Boolean) = new AgentExtractor {
+    val test = pf
+  }
   
   // Agent extractors.
   val Chrome  = agent(chromeAM)
