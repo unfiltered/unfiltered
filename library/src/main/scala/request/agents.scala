7423f56febfad8aa5962d0d2c1c53605f476bd8a
Chris Lewis <chris@thegodcode.net>
2011-08-07 13:41:34 -0400

    reorg

--- library/src/main/scala/request/agents.scala
+++ library/src/main/scala/request/agents.scala
@@ -35,11 +35,13 @@ object AgentIs {
   type AM = String => Boolean
   
   // Matchers for major agents.
-  val chromeAM: AM  = _.contains("Chrome")
-  val firefoxAM: AM = _.toLowerCase.contains("firefox")
-  val operaAM: AM   = _.contains("Opera")
-  val ieAM: AM      = (ua) => ua.contains("MSIE") && !ua.contains("Opera")
-  val safariAM: AM  = (ua) => (ua.contains("AppleWebKit") || ua.contains("Safari")) && ! ua.contains("Chrome")
+  val chromeAM:   AM = _.contains("Chrome")
+  val firefoxAM:  AM = _.toLowerCase.contains("firefox")
+  val operaAM:    AM = _.contains("Opera")
+  val ieAM:       AM = (ua) => ua.contains("MSIE") && !ua.contains("Opera")
+  val safariAM:   AM = (ua) => (ua.contains("AppleWebKit") || ua.contains("Safari")) && ! ua.contains("Chrome")
+  // Mobile.
+  val mobile:     AM = (ua) => MobileAgent.all.exists(ua.contains(_))
   
   def agent[A](pf: String => Boolean) = new AgentExtractor { val test = pf }
   
@@ -49,13 +51,14 @@ object AgentIs {
   val FireFox = agent(firefoxAM)
   val IE      = agent(ieAM)
   val Opera   = agent(operaAM)
-  // Mobile.
-  val Mobile  = agent(ua => MobileAgent.all.exists(ua.contains(_)))
-  val SafariMobile = Safari & Mobile
+  
+  // Mobile extractors.
+  val Mobile        = agent(mobile)
+  val SafariMobile  = Safari & Mobile
   
 }
 
 object MobileAgent {
-  val appleMobile = List("iPhone", "iPod", "iPad")
-  val all = appleMobile
+  val apple = List("iPhone", "iPod", "iPad")
+  val all = apple
 }
