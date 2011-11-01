db02a0446f8cfbada110b6b41fd7231c6db890c8
Daniel Westheide <dwestheide@googlemail.com>
2011-11-01 15:54:45 +0100

    Better alternative auth scheme support in resource server.
    Improved support for alternative authentication schemes on
    the resource server side, most notably for the MAC scheme,
    which also requires different use of attributes in the
    WWW-Authenticate header.
    
    By only having the MacAuth in the list of supported schemes,
    it's now possible to inform clients correctly about the
    required authentication scheme, instead of always returning
    the "Bearer" challenge when no token was provided at all.
    
    Also, adding some additional tests to ensure we are in line
    with the current specs for OAuth2 as well as Bearer and Mac
    schemes.

--- oauth2/src/test/scala/ProtectionSpec.scala
+++ oauth2/src/test/scala/ProtectionSpec.scala
@@ -65,11 +65,35 @@ object ProtectionSpec extends Specification with unfiltered.spec.jetty.Served {
       }
     }
 
+    // see http://tools.ietf.org/html/draft-ietf-oauth-v2-bearer-08#section-2.4.1
     "fail on a bad Bearer header" in {
       val bearer_header = Map("Authorization" -> "Bearer bad_token")
       println(host / "user" <:< bearer_header as_str)
       Http.when(_ == 401)(host / "user" <:< bearer_header as_str) must_== """error="%s" error_description="%s" """.trim.format("invalid_token", "bad token")
+      val head = Http.when(_ == 401)(host / "user" <:< bearer_header >:> { h => h })
+      head.get("WWW-Authenticate") must_!= (None)
+      val wwwAuth = head("WWW-Authenticate")
+      val expected = "Bearer error=\"%s\",error_description=\"%s\"".trim.format("invalid_token", "bad token")
+      wwwAuth.head must_== expected
     }
+
+    // see http://tools.ietf.org/html/draft-ietf-oauth-v2-bearer-08#section-2.4.1
+    "fail without returning an error code and description on missing authentication information" in {
+      val head = Http.when(_ == 401)(host / "user" >:> { h => h })
+      head.get("WWW-Authenticate") must_!= (None)
+      val wwwAuth = head("WWW-Authenticate")
+      wwwAuth.head must_== "Bearer"
+    }
+    // see http://tools.ietf.org/html/draft-ietf-oauth-v2-http-mac-00#section-4.1
+    "fail on a bad MAC header" in {
+      val macHeader = Map("Authorization" -> """MAC id="bogus",nonce="123:y",bodyhash="bogus",mac="bogus"""")
+      val head = Http.when(_ == 401)(host / "user" <:< macHeader >:> { h => h })
+      head.get("WWW-Authenticate") must_!= (None)
+      val wwwAuth = head("WWW-Authenticate")
+      val expected = "MAC error=\"%s\"".trim.format("invalid token")
+      wwwAuth.head must_== expected
+    }
+
 /*
     "authenticate a valid access token via MAC header" in {
       val mac_header = Map("Authorization" -> """MAC id="%s",nonce="123:x",bodyhash="x",mac="x" """.format(GoodMacToken).trim)
