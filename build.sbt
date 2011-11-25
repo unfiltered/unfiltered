1057c9ed0dea0469fecc2f4b3a9aa035dabd8710
Nathan Hamblen <nathan@technically.us>
2011-11-24 22:35:04 -0500

    ls 0.1.0

---
+++ build.sbt
@@ -0,0 +1,3 @@
+
+
+seq(lsSettings :_*)
--- project/project/build.scala
+++
@@ -1,5 +0,0 @@
-import sbt._
-object PluginDef extends Build {
-  lazy val root = Project("plugins", file(".")) dependsOn( plugin )
-  lazy val plugin = ProjectRef(file("../../ls"), "plugin")
-}
