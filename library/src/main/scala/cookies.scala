e1f2e332329c1693cac9f5620ee867dd8aa311ba
softprops <d.tangren@gmail.com>
2011-12-02 20:56:19 -0500

    drop deprecated cookie methods

--- library/src/main/scala/cookies.scala
+++ library/src/main/scala/cookies.scala
@@ -34,13 +34,4 @@ object CookieKeys {
 case class Cookie(name: String, value: String, domain: Option[String] = None,
                   path: Option[String] = None, maxAge: Option[Int] = None,
                   secure: Option[Boolean] = None, httpOnly: Boolean = false,
-                  version: Int = 0) {
-  @deprecated("use copy(domain = Some(d))")
-  def domain(d: String): Cookie = copy(domain = Some(d))
-  @deprecated("use copy(path=Some(p))")
-  def path(p: String): Cookie = copy(path = Some(p))
-  @deprecated("use copy(maxAge = Some(a))")
-  def maxAge(a: Int): Cookie = copy(maxAge = Some(a))
-  @deprecated("use copy(secure = Some(s))")
-  def secure(s: Boolean): Cookie = copy(secure = Some(s))
-}
+                  version: Int = 0)
