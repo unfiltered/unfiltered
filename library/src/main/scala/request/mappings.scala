7b3240d899650d02e407314bf01c85a4ededeadf
Nathan Hamblen <nathan@technically.us>
2012-02-21 01:46:02 -0500

    some factoring out

--- library/src/main/scala/request/mappings.scala
+++ library/src/main/scala/request/mappings.scala
@@ -6,32 +6,35 @@ import unfiltered.response._
 import scala.util.matching.Regex
 
 object PathMap {
-  def startsWith[A, B]
-  (route: (String, ((HttpRequest[A], String) => ResponseFunction[B]))*)
-  : unfiltered.Cycle.Intent[A, B] = { case req =>
-    val path = Path(req)
-    route.find {
-      case (k, _) => path.startsWith(k)
-    }.map {
-      case (k, rf) => rf(req, path.substring(k.length))
-    }.getOrElse { Pass }
+
+  def toIntent[A,B,K,F](route: Traversable[(K,F)])(
+    f: (HttpRequest[A], String, K, F) => Option[ResponseFunction[B]]
+  ): unfiltered.Cycle.Intent[A, B] = {
+    case req @ Path(path) =>
+      route.view.flatMap { case (key, handler) =>
+        f(req, path, key, handler)
+      }.filter { _ != Pass }.headOption.getOrElse { Pass }
   }
 
-  def matching[A, B]
-  (route: (String, ((HttpRequest[A], Regex.Match) => ResponseFunction[B]))*)
-  : unfiltered.Cycle.Intent[A, B] = {
-    val regexRoute = route.map { case (k, v) => k.r -> v }
-    ({
-      case req =>
-        val path = Path(req)
-        regexRoute.view.flatMap {
-          case (regex, rf) =>
-            regex.findPrefixMatchOf(path).map { mtch =>
-              rf(req, mtch)
-            }
-        }.headOption.getOrElse { Pass }
-    })
-  }
+  def startsWith[A,B](
+    route: (String, (HttpRequest[A], String) => ResponseFunction[B])*
+  ) =
+    toIntent(route) { (req: HttpRequest[A], path, k, rf) =>
+      if (path.startsWith(k))
+        Some(rf(req, path.substring(k.length)))
+      else None
+    }
+
+  def regex[A, B](
+    route: (String, ((HttpRequest[A], Regex.Match) => ResponseFunction[B]))*
+  ) =
+    toIntent(
+      route.map { case (k, v) => k.r -> v }
+    ) { (req: HttpRequest[A], path, regex, rf) =>
+      regex.findPrefixMatchOf(path).map { mtch =>
+        rf(req, mtch)
+      }
+    }
 }
 
 
@@ -47,12 +50,12 @@ object Test  {
     error("todo")
 
   def matchIntent[A,B]: unfiltered.Cycle.Intent[A,B] =
-    PathMap.matching(
-      """/name/(\d+)""" -> Test.matchMethod,
-      "/def"            -> Test.matchMethod
+    PathMap.regex(
+      """/name/(\d+)""" -> Test.regexMethod,
+      "/def"            -> Test.regexMethod
     )
 
-  def matchMethod[A,B](req: HttpRequest[A], mtch: Regex.Match):
+  def regexMethod[A,B](req: HttpRequest[A], mtch: Regex.Match):
   ResponseFunction[B] =
     error("todo")
 }
