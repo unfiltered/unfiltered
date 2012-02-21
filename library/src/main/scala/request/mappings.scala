039a25ca942496b10dc039df4bfefa6faf9c6fea
Nathan Hamblen <nathan@technically.us>
2012-02-20 23:52:17 -0500

    regex mapper

--- library/src/main/scala/request/mappings.scala
+++ library/src/main/scala/request/mappings.scala
@@ -3,35 +3,56 @@ package unfiltered.kit
 import unfiltered.request._
 import unfiltered.response._
 
+import scala.util.matching.Regex
+
 object PathMap {
   def startsWith[A, B]
-  (route: (String, (HttpRequest[A] => ResponseFunction[B]))*)
-  : PartialFunction[HttpRequest[A], ResponseFunction[B]] = { case req =>
+  (route: (String, ((HttpRequest[A], String) => ResponseFunction[B]))*)
+  : unfiltered.Cycle.Intent[A, B] = { case req =>
     val path = Path(req)
     route.find {
       case (k, _) => path.startsWith(k)
     }.map {
-      case (_, rf) => rf(req)
+      case (k, rf) => rf(req, path.substring(k.length))
     }.getOrElse { Pass }
+  }
+
+  def matching[A, B]
+  (route: (String, ((HttpRequest[A], Regex.Match) => ResponseFunction[B]))*)
+  : unfiltered.Cycle.Intent[A, B] = {
+    val regexRoute = route.map { case (k, v) => k.r -> v }
+    ({
+      case req =>
+        val path = Path(req)
+        regexRoute.view.flatMap {
+          case (regex, rf) =>
+            regex.findPrefixMatchOf(path).map { mtch =>
+              rf(req, mtch)
+            }
+        }.headOption.getOrElse { Pass }
+    })
   }
 }
 
 
-object Test extends  {
-  def intent[A,B]: unfiltered.Cycle.Intent[A,B] =
+object Test  {
+  def stringIntent[A,B]: unfiltered.Cycle.Intent[A,B] =
     PathMap.startsWith(
-      "/abc" -> Test.extractionMethod
+      "/abc" -> Test.stringMethod,
+      "/def" -> Test.stringMethod
     )
 
-  def asyncIntent[A,B]: unfiltered.Async.Intent[A,B] =
-    PathMap.startsWith(
-      "/abc" -> Test.extractionMethod _
+  def stringMethod[A,B](req: HttpRequest[A], remainder: String):
+  ResponseFunction[B] =
+    error("todo")
+
+  def matchIntent[A,B]: unfiltered.Cycle.Intent[A,B] =
+    PathMap.matching(
+      """/name/(\d+)""" -> Test.matchMethod,
+      "/def"            -> Test.matchMethod
     )
 
-  def extractionMethod[A,B](req: HttpRequest[A]): ResponseFunction[B] =
+  def matchMethod[A,B](req: HttpRequest[A], mtch: Regex.Match):
+  ResponseFunction[B] =
     error("todo")
-
-  def asyncExtractionMethod[A,B](req: HttpRequest[A] with Responder[B]): Any =
-    error("todo")
-
 }
