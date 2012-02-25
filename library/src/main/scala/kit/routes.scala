e6ef817d61b2969359f7a573875641af4f504e8d
Nathan Hamblen <nathan@technically.us>
2012-02-25 10:05:00 -0500

    Routes.specify specs

--- library/src/main/scala/kit/routes.scala
+++ library/src/main/scala/kit/routes.scala
@@ -5,7 +5,7 @@ import unfiltered.response._
 
 import scala.util.matching.Regex
 
-object PathMap {
+object Routes {
 
   def toIntent[A,B,K,F](route: Traversable[(K,F)])(
     f: (HttpRequest[A], String, K, F) => Option[ResponseFunction[B]]
@@ -61,41 +61,4 @@ object PathMap {
         }
       }
     }
-}
-
-
-object Test  {
-  def stringIntent[A,B]: unfiltered.Cycle.Intent[A,B] =
-    PathMap.startsWith(
-      "/abc" -> Test.stringMethod,
-      "/def" -> Test.stringMethod
-    )
-
-  def stringMethod[A,B](req: HttpRequest[A], remainder: String):
-  ResponseFunction[B] =
-    ResponseString("hi")
-
-  def regexIntent[A,B]: unfiltered.Cycle.Intent[A,B] =
-    PathMap.regex(
-      """/name/(\d+)""" -> Test.regexMethod,
-      "/def"            -> Test.regexMethod
-    )
-
-  def regexMethod[A,B](req: HttpRequest[A], mtch: Regex.Match):
-  ResponseFunction[B] =
-    ResponseString("hi")
-
-  def specIntent[A,B]: unfiltered.Cycle.Intent[A,B] =
-    PathMap.specify(
-      "/things/:id" -> Test.things,
-      "/things"     -> Test.allTheThings
-    )
-
-  def things[A,B](req: HttpRequest[A], params: Map[String,String]) = {
-    val id = params("id")
-    ResponseString("hi")
-  }
-
-  def allTheThings[A,B](req: HttpRequest[A], params: Map[String,String]) =
-    ResponseString("hi")
 }
---
+++ library/src/test/scala/RoutesSpec.scala
@@ -0,0 +1,65 @@
+package unfiltered.kit
+
+import org.specs._
+
+import unfiltered.request._
+
+object RoutesSpecJetty
+extends unfiltered.spec.jetty.Planned
+with RoutesSpec
+
+object RoutesSpecNetty
+extends unfiltered.spec.netty.Planned
+with RoutesSpec
+
+trait RoutesSpec extends unfiltered.spec.Hosted {
+  import unfiltered.response._
+  import unfiltered.request.{Path => UFPath}
+  import unfiltered.Cookie
+
+  import dispatch._
+
+  def intent[A,B]: unfiltered.Cycle.Intent[A,B] = Routes.specify(
+    "/widgets" -> widgets,
+    "/widgets/:widget_id" -> getWidget,
+    "/widgets/:widget_id/sprockets" -> sprockets,
+    "/widgets/:widget_id/sprockets/:sprocket_id" -> assembly
+  )
+
+  def widgets[A](req: HttpRequest[A], params: Map[String, String]) = {
+    ResponseString("allTheWidgets")
+  }
+
+  def getWidget[A](req: HttpRequest[A], params: Map[String, String]) = {
+    ResponseString("widget:%s".format(params("widget_id")))
+  }
+
+  def sprockets[A](req: HttpRequest[A], params: Map[String, String]) = {
+    ResponseString("sprocketsOf:%s".format(params("widget_id")))
+  }
+
+  def assembly[A](req: HttpRequest[A], params: Map[String, String]) = {
+    val widgetId :: sprocketId :: Nil =
+      ("widget_id" :: "sprocket_id" :: Nil).map(params)
+    ResponseString("%s:%s".format(widgetId, sprocketId))
+  }
+
+  "Routes.specify" should {
+    "match a path" in {
+      http(host / "widgets" as_str) must_==
+        "allTheWidgets"
+    }
+    "match a parameter" in {
+      http(host / "widgets" / "123" as_str) must_==
+        "widget:123"
+    }
+    "match a parameter and path" in {
+      http(host / "widgets" / "123" / "sprockets" as_str) must_==
+        "sprocketsOf:123"
+    }
+    "match two parameters" in {
+      http(host / "widgets" / "123" / "sprockets" / "456" as_str) must_==
+        "123:456"
+    }
+  }
+}
