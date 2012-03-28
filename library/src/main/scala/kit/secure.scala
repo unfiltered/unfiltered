a74507cff4d5cae57157f16d14adef4c465fc3bb
Nathan Hamblen <nathan@technically.us>
2012-03-27 23:19:09 -0400

    secure port handling and tests

--- library/src/main/scala/kit/secure.scala
+++ library/src/main/scala/kit/secure.scala
@@ -4,13 +4,20 @@ import unfiltered.request._
 import unfiltered.response._
 
 object Secure {
-  def redir[A,B](intent: unfiltered.Cycle.Intent[A,B]) = {
+  def redir[A,B](intent: unfiltered.Cycle.Intent[A,B],
+                 port: Int = -1) = {
     intent.fold(
       { _ => Pass },
       {
         case (HTTPS(req), rf) => rf
         case (req @ Host(host), rf) =>
-          Redirect("https://%s%s".format(host, req.uri))
+          val h = host.split(':')(0)
+          Redirect(
+            if (port > -1)
+              "https://%s:%d%s".format(h, port, req.uri)
+            else
+              "https://%s%s".format(h, req.uri)
+          )
       }
     )
   }
