93f744684ff7662edd71028ee7f95f82a05912f0
Nathan Hamblen <nathan@technically.us>
2012-03-31 13:02:04 -0400

    scala 2.9.1-1 support

--- project/build.scala
+++ project/build.scala
@@ -9,7 +9,7 @@ object Shared {
   val continuation = "org.eclipse.jetty" % "jetty-continuation" % jettyVersion % "compile"
 
   def specsDep(sv: String) =
-    sv.split('.').toList match {
+    sv.split("[.-]").toList match {
       case "2" :: "8" :: _ => "org.scala-tools.testing" % "specs_2.8.1" % "1.6.8"
       case "2" :: "9" :: "1" :: _ => "org.scala-tools.testing" % "specs_2.9.1" % "1.6.9"
       case "2" :: "9" :: _ => "org.scala-tools.testing" %% "specs" % "1.6.8"
@@ -258,8 +258,12 @@ object Unfiltered extends Build {
            fullClasspath in (local("filter"), Compile)) map { (s, f) =>
              s ++ f
           },
-        libraryDependencies <++= scalaVersion(
-          Seq("net.liftweb" %% "lift-json" % "2.4") ++ integrationTestDeps(_))
+        libraryDependencies <++= scalaVersion( sv =>
+          Seq(sv.split("[.-]").toList match {
+            case "2" :: "9" :: "1" :: _ =>
+              "net.liftweb" % "lift-json_2.9.1" % "2.4"
+            case _ => "net.liftweb" %% "lift-json" % "2.4"
+          }) ++ integrationTestDeps(sv))
       )) dependsOn(library)
 
   lazy val websockets =
