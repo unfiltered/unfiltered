4b2b9429c4bace76e3577876c0d9b32d6d3ef8de
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-04-27 22:50:12 +0200

    Conneg headers needs to be sorted by their qualifier. No clue if one should change Accepts.Accepting, or create a new kit to make this more usable

---
+++ util/src/main/scala/mime.scala
@@ -0,0 +1,54 @@
+package unfiltered.util
+
+case class MIMEType(major: String, minor: String, params: Map[String, String] = Map.empty) {
+  def includes(mt: MIMEType) = {
+     this match {
+	   case MIMEType.ALL => true
+	   case MIMEType(maj, "*", _) => mt.major == maj
+	   case MIMEType(maj, min, _) => mt.major == maj && mt.minor == min
+     }	
+  }
+  override def toString = "%s/%s".format(major, minor) + params.map{case (a, b) => "; %s=%s".format(a, b)}.mkString("")
+}
+
+object MIMEType {
+  val ALL = MIMEType("*", "*")
+
+  val APPLICATION_ALL = MIMEType("application", "*")  
+  val APPLICATION_ATOM = MIMEType("application", "atom+xml")
+  val APPLICATION_ATOM_CATEGORY = MIMEType("application", "atomcat+xml")
+  val APPLICATION_ATOM_SERVICE = MIMEType("application", "atomsvc+xml")
+  val APPLICATION_OCTET_STREAM = MIMEType("application", "octet-stream")
+  val APPLICATION_FORM_URLENCODED = MIMEType("application", "x-www-form-urlencoded")
+  val APPLICATION_XML = MIMEType("application", "xml")  
+
+  val IMAGE_ALL = MIMEType("image", "*")
+  val IMAGE_JPEG = MIMEType("image", "jpeg")
+  val IMAGE_PNG = MIMEType("image", "png")
+
+  val MULTIPART_FORM_DATA = MIMEType("multipart", "form-data")
+
+  val TEXT_HTML = MIMEType("text", "html")
+  val TEXT_PLAIN = MIMEType("text", "plain")
+  val TEXT_URI_LIST = MIMEType("text", "uri-list")
+  
+  val VIDEO_ALL = MIMEType("video", "*")
+  val VIDEO_MP4 = MIMEType("video", "mp4")
+  val VIDEO_MPEG = MIMEType("video", "mpeg")
+
+  def apply(mt: String): Option[MIMEType] = {
+    import javax.activation.MimeType
+    import scala.collection.JavaConverters._
+    try {
+      val mimeType = new MimeType(mt)
+      val names = mimeType.getParameters.getNames.asInstanceOf[java.util.Enumeration[String]].asScala
+      val params = names.foldLeft(Map[String, String]())((acc, p) => acc + (p -> mimeType.getParameter(p)))
+      Some(new MIMEType(mimeType.getPrimaryType, mimeType.getSubType, params))
+    }
+    catch {
+      case _ => None
+    }
+  }
+
+  def string2MIMEType(input: String) = apply(input).getOrElse(throw new IllegalArgumentException("Not a valid MIMEType"))
+}
