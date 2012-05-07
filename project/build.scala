18727961bac9ee7986f83b70620e5328deb37c26
Nathan Hamblen <nathan@technically.us>
2012-05-06 23:57:09 -0400

    0.6.2

--- project/build.scala
+++ project/build.scala
@@ -37,7 +37,7 @@ object Unfiltered extends Build {
     ls.Plugin.lsSettings ++
     Seq(
     organization := "net.databinder",
-    version := "0.6.2-SNAPSHOT",
+    version := "0.6.2",
     crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2",
                               "2.9.0", "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2"),
     scalaVersion := "2.8.2",
