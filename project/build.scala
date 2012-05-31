1bc3c75ed976361f2f1dd8ffc295b698f7181017
Nathan Hamblen <nathan@technically.us>
2012-05-30 21:44:04 -0400

    drop some old releases of scala

--- project/build.scala
+++ project/build.scala
@@ -38,8 +38,8 @@ object Unfiltered extends Build {
     Seq(
     organization := "net.databinder",
     version := "0.6.2",
-    crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2",
-                              "2.9.0", "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2"),
+    crossScalaVersions := Seq("2.8.1", "2.8.2",
+                              "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2"),
     scalaVersion := "2.8.2",
     publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
