9eff35ea1df1a37a09c481cf4623ef6ea330b696
Carsten Saathoff <carsten@kreuzverweis.com>
2012-12-17 17:52:27 +0100

    reverted version and added back netty-uploads to project definition

--- project/build.scala
+++ project/build.scala
@@ -67,8 +67,7 @@ object Unfiltered extends Build {
   lazy val unfiltered =
     Project("unfiltered-all", file(".")).delegateTo(setup).aggregate(
             library, filters, filtersAsync , uploads, filterUploads,
-            //nettyUploads, 
-	    util, jetty,
+            nettyUploads, util, jetty,
             jettyAjpProject, netty, nettyServer, json, specHelpers,
             scalaTestHelpers, websockets, oauth,  mac,
             oauth2, agents)
