277e00a5cfb2bc835be6f1f34e59b0f7c4a6dc1f
softprops <d.tangren@gmail.com>
2013-12-10 08:27:02 -0500

    make secured trait sharable as it maintains no state. also be sure to pass custom user events upstream

--- netty/src/main/scala/secured.scala
+++ netty/src/main/scala/secured.scala
@@ -2,19 +2,23 @@ package unfiltered.netty
 
 import io.netty.channel.{ ChannelHandlerContext, ChannelInboundHandlerAdapter }
 import io.netty.handler.ssl.{ SslHandler, SslHandshakeCompletionEvent }
+import io.netty.channel.ChannelHandler.Sharable
 
 /** Adds ssl handshaking to a channel handler's #channelConnected method
   * This assumes a SslHandler was added to the underlying ChannelPipeline */
+@Sharable // this indicates that the handler is stateless and be called without syncronization
 trait Secured extends ChannelInboundHandlerAdapter {
   /** Client code should always pass userEventTriggered events upstream */
-  override def userEventTriggered(ctx: ChannelHandlerContext, msg: java.lang.Object): Unit =
-    msg match {
+  override def userEventTriggered(ctx: ChannelHandlerContext, event: java.lang.Object): Unit =
+    event match {
       case complete: SslHandshakeCompletionEvent =>
         channelSecured(ctx, complete)
-      case _ => ()
+      case evt =>
+        ctx.fireUserEventTriggered(evt)
     }
 
-  /** Called after a successful Ssl handshake. By default, this does nothing. 
+  /** Called after a successful Ssl handshake. By default, this does nothing but pass the event upstream
     * Override this for post-handshake behavior. */
-  def channelSecured(ctx: ChannelHandlerContext, complete: SslHandshakeCompletionEvent): Unit = ()
+  def channelSecured(ctx: ChannelHandlerContext, complete: SslHandshakeCompletionEvent): Unit =
+    ctx.fireUserEventTriggered(complete)
 }
