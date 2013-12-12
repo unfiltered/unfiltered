cab19581dd74dbc3df955d3e6399fb485f822714
softprops <d.tangren@gmail.com>
2013-12-12 00:49:57 -0500

    cleanup, when closing netty response output stream, clear the content before writing to avoid duplicate content

--- netty/src/main/scala/bindings.scala
+++ netty/src/main/scala/bindings.scala
@@ -2,16 +2,18 @@ package unfiltered.netty
 
 import unfiltered.{ Async, Cookie }
 import unfiltered.response.{ ResponseFunction, HttpResponse, Pass }
-import unfiltered.request.{ HttpRequest,POST,PUT,&,RequestContentType,Charset }
+import unfiltered.request.{ HttpRequest, POST, PUT, &, RequestContentType, Charset }
 
 import io.netty.buffer.{ Unpooled, ByteBufInputStream }
-import io.netty.channel.{ ChannelFutureListener, ChannelHandlerContext }
-import io.netty.handler.codec.http.{ DefaultFullHttpResponse, FullHttpRequest, FullHttpResponse, HttpHeaders, HttpResponseStatus, HttpVersion }
-import io.netty.handler.codec.http.{ HttpResponse => NHttpResponse,                                    
-                                     HttpRequest  => NHttpRequest }
+import io.netty.channel.{ ChannelFuture, ChannelFutureListener, ChannelHandlerContext }
+import io.netty.handler.codec.http.{
+  DefaultFullHttpResponse, FullHttpRequest, FullHttpResponse, HttpContent,
+  HttpHeaders, HttpRequest => NettyHttpRequest, HttpResponse => NHttpResponse,
+  HttpResponseStatus, HttpVersion }
 import io.netty.handler.ssl.SslHandler
+
 import java.io.{ BufferedReader, ByteArrayOutputStream, InputStreamReader }
-import java.net.URLDecoder
+import java.net.{ InetSocketAddress, URLDecoder }
 import java.nio.charset.{ Charset => JNIOCharset }
 
 import scala.collection.JavaConverters._
@@ -25,6 +27,8 @@ class RequestBinding(msg: ReceivedMessage)
 
   private val req = msg.request
 
+  private val content = msg.content
+
   private lazy val params = queryParams ++ bodyParams
 
   private def queryParams = req.getUri.split("\\?", 2) match {
@@ -32,18 +36,20 @@ class RequestBinding(msg: ReceivedMessage)
     case _ => Map.empty[String,Seq[String]]
   }
 
-  private def bodyParams = this match {
-    case (POST(_) | PUT(_)) & RequestContentType(ct)
+  private def bodyParams = (this, content) match {
+    case ((POST(_) | PUT(_)) & RequestContentType(ct), Some(content))
       if ct.contains("application/x-www-form-urlencoded") =>
-      URLParser.urldecode(req.content.toString(JNIOCharset.forName(charset)))
-    case _ => Map.empty[String,Seq[String]]
+      URLParser.urldecode(content.content.toString(JNIOCharset.forName(charset)))
+    case _ =>
+      Map.empty[String,Seq[String]]
   }
 
   private def charset = Charset(this).getOrElse {
     HttpConfig.DEFAULT_CHARSET
   }
 
-  lazy val inputStream = new ByteBufInputStream(req.content)
+  lazy val inputStream =
+    new ByteBufInputStream(content.map(_.content).getOrElse(Unpooled.EMPTY_BUFFER))
 
   lazy val reader =
     new BufferedReader(new InputStreamReader(inputStream, charset))
@@ -70,7 +76,7 @@ class RequestBinding(msg: ReceivedMessage)
   def isSecure =
     Option(msg.context.pipeline.get(classOf[SslHandler])).isDefined
 
-  def remoteAddr = msg.context.channel.remoteAddress.asInstanceOf[java.net.InetSocketAddress].getAddress.getHostAddress
+  def remoteAddr = msg.context.channel.remoteAddress.asInstanceOf[InetSocketAddress].getAddress.getHostAddress
 
   def respond(rf: ResponseFunction[FullHttpResponse]) =
     underlying.respond(rf)
@@ -78,9 +84,15 @@ class RequestBinding(msg: ReceivedMessage)
 
 /** Extension of basic request binding to expose Netty-specific attributes */
 case class ReceivedMessage(
-  request: FullHttpRequest,
+  request: NettyHttpRequest,
   context: ChannelHandlerContext,
   message: java.lang.Object) { // todo: remove this. its the same as request?
+
+  def content: Option[HttpContent] =
+    request match {
+      case has: HttpContent => Some(has)
+      case not => None
+    }
 
   /** Binds a Netty HttpResponse res to Unfiltered's HttpResponse to apply any
    * response function to it. */
@@ -88,11 +100,12 @@ case class ReceivedMessage(
     rf(new ResponseBinding(res)).underlying
 
   /** @return a new Netty FullHttpResponse bound to an Unfiltered HttpResponse */
-  val defaultResponse = response(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK))_
+  lazy val defaultResponse = response(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK))_
 
   /** Applies rf to a new `defaultResponse` and writes it out */
   def respond: (ResponseFunction[FullHttpResponse] => Unit) = {
-    case Pass => context.fireChannelRead(request)
+    case Pass =>
+      context.fireChannelRead(request)
     case rf =>
       val keepAlive = HttpHeaders.isKeepAlive(request)
       val closer = new unfiltered.response.Responder[FullHttpResponse] {
@@ -107,9 +120,13 @@ case class ReceivedMessage(
           )(res)
         }
       }
-      val future = context.channel.write(
+      val future = context.channel.writeAndFlush(
         defaultResponse(rf ~> closer)
-      )
+      ).addListener(new ChannelFutureListener {
+        def operationComplete(f: ChannelFuture) {
+          content.map(_.release())
+        }
+      })
       if (!keepAlive)
         future.addListener(ChannelFutureListener.CLOSE)
   }
@@ -118,25 +135,26 @@ case class ReceivedMessage(
 class ResponseBinding[U <: FullHttpResponse](res: U)
     extends HttpResponse(res) {
   private lazy val byteOutputStream = new ByteArrayOutputStream {
-    override def close =
-      res.content.writeBytes(Unpooled.copiedBuffer(this.toByteArray))
+    // fixme: the docs state http://docs.oracle.com/javase/6/docs/api/java/io/ByteArrayOutputStream.html#close()
+    //  should have no effect. we are breaking that rule here if close is called more than once
+    override def close = {
+      res.content.clear().writeBytes(Unpooled.copiedBuffer(this.toByteArray))
+    }
   }
 
-  def status(statusCode: Int) =
-    res.setStatus(HttpResponseStatus.valueOf(statusCode))
+  def status(code: Int) =
+    res.setStatus(HttpResponseStatus.valueOf(code))
 
-  def header(name: String, value: String) = res.headers.add(name, value)
+  def header(name: String, value: String) =
+    res.headers.add(name, value)
 
-  def redirect(url: String) = {
-    res.setStatus(HttpResponseStatus.FOUND)
-    res.headers.add(HttpHeaders.Names.LOCATION, url)
-  }
+  def redirect(url: String) =
+    res.setStatus(HttpResponseStatus.FOUND).headers.add(HttpHeaders.Names.LOCATION, url)
 
   def outputStream = byteOutputStream
 }
 
 private [netty] object URLParser {
-
   def urldecode(enc: String) : Map[String, Seq[String]] = {
     def decode(raw: String) = URLDecoder.decode(raw, HttpConfig.DEFAULT_CHARSET)
     val pairs = enc.split('&').flatMap {
@@ -150,5 +168,4 @@ private [netty] object URLParser {
       case (m, (k, v)) => m + (k -> (v :: m(k)))
     }
   }
-
 }
