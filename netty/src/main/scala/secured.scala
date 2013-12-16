03aef58262f6ff88570664e200378d94bc9df304
softprops <d.tangren@gmail.com>
2013-12-15 20:00:11 -0500

    handshake is new called for you. document that and provide separate hooks for getting notified about handshake success and failures

--- netty/src/main/scala/secured.scala
+++ netty/src/main/scala/secured.scala
@@ -1,24 +1,31 @@
 package unfiltered.netty
 
-import io.netty.channel.{ ChannelHandlerContext, ChannelInboundHandlerAdapter }
+import io.netty.channel.{ Channel, ChannelFuture, ChannelFutureListener, ChannelHandlerContext, ChannelInboundHandlerAdapter }
 import io.netty.handler.ssl.{ SslHandler, SslHandshakeCompletionEvent }
 import io.netty.channel.ChannelHandler.Sharable
+import io.netty.util.concurrent.{ Future, GenericFutureListener }
 
 /** Adds ssl handshaking to a channel handler's #channelConnected method
-  * This assumes a SslHandler was added to the underlying ChannelPipeline */
-@Sharable // this indicates that the handler is stateless and be called without syncronization
+  * This assumes a SslHandler was added to the underlying ChannelPipeline.
+  * The ssl handshake will be handled for you. You may wish to know preform custom behavior on
+  * if the handshake is a pass or failure. To do so, use the handshakeSuccess(ctx) or handshakeFailure(ctx, event)
+  * to do so */
+@Sharable // this indicates that the handler is stateless and be called without synchronization
 trait Secured extends ChannelInboundHandlerAdapter {
-  /** Client code should always pass userEventTriggered events upstream */
+  /** Client code should _always_ pass userEventTriggered events upstream */
   override def userEventTriggered(ctx: ChannelHandlerContext, event: java.lang.Object): Unit =
     event match {
       case complete: SslHandshakeCompletionEvent =>
-        channelSecured(ctx, complete)
+        if (complete.isSuccess) handshakeSuccess(ctx)
+        else handshakeFailure(ctx, complete)
       case evt =>
         ctx.fireUserEventTriggered(evt)
     }
 
-  /** Called after a successful Ssl handshake. By default, this does nothing but pass the event upstream
-    * Override this for post-handshake behavior. */
-  def channelSecured(ctx: ChannelHandlerContext, complete: SslHandshakeCompletionEvent): Unit =
-    ctx.fireUserEventTriggered(complete)
+  /** Called after a successful ssl handshake attempt */
+  def handshakeSuccess(ctx: ChannelHandlerContext): Unit = {}
+
+  /** Called after a failed ssl handshake attempt */
+  def handshakeFailure(ctx: ChannelHandlerContext, event: SslHandshakeCompletionEvent): Unit = {}
+
 }
