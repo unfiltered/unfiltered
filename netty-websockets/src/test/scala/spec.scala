dd0d0ccb82170a945f8c98e6d890ee02107529f8
softprops <d.tangren@gmail.com>
2013-12-16 08:42:39 -0500

    websocket tests are passing

--- netty-websockets/src/test/scala/spec.scala
+++ netty-websockets/src/test/scala/spec.scala
@@ -1,16 +1,19 @@
 package unfiltered.netty.websockets
 
+import com.ning.http.client.providers.netty.NettyAsyncHttpProviderConfig
+import unfiltered.request.{ Path => UFPath, _ }
+import tubesocks.{
+  Open => TOpen,
+  Close => TClose,
+  Message => TMessage,
+  Error => TError,
+  _
+}
+import java.net.URI
+import java.util.concurrent.{ CountDownLatch, TimeUnit }
+import scala.collection.mutable
+
 object WebSocketPlanSpec extends unfiltered.spec.netty.Served {
-  import unfiltered.request.{ Path => UFPath, _ }
-
-  import tubesocks.{
-    Open => TOpen,
-    Close => TClose,
-    Message => TMessage,
-    Error => TError,
-    _
-  }
-
   def setup = _.handler(Planify({
     case GET(UFPath("/")) => {
       case Open(s) =>
@@ -24,33 +27,25 @@ object WebSocketPlanSpec extends unfiltered.spec.netty.Served {
 
   "A websocket server" should {
     "accept connections" in {
-      import java.util.concurrent.{ CountDownLatch, TimeUnit }
-      var m = scala.collection.mutable.Map.empty[String, String]
-      val l = new CountDownLatch(1)
+      var m = mutable.Map.empty[String, String]
       Sock.uri(wsuri) {
         case TOpen(s) =>
           s.send("open")
         case TMessage(t, _) =>
           m += ("rec" -> t)
-          l.countDown
       }
-      l.await(4, TimeUnit.MILLISECONDS)
-      m must havePair(("rec", "open"))
+      m must havePair(("rec", "open")).eventually
     }
 
     "handle messages" in {
-      import java.util.concurrent.{ CountDownLatch, TimeUnit }
-      var m = scala.collection.mutable.Map.empty[String, String]
-      val l = new CountDownLatch(2)
+      var m = mutable.Map.empty[String, String]
       Sock.uri(wsuri) {
         case TOpen(s) =>
           s.send("from client")
         case TMessage(t, _) =>
           m += ("rec" -> t)
-          l.countDown
       }
-      l.await(4, TimeUnit.MILLISECONDS)
-      m must havePair(("rec", "from client"))
+      m must havePair(("rec", "from client")).eventually
     }
   }
 }
