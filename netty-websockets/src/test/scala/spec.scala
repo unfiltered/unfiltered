ef1520e0c3aad7013a3375d467f83df101e97e32
softprops <d.tangren@gmail.com>
2013-12-16 23:21:02 -0500

    classify

--- netty-websockets/src/test/scala/spec.scala
+++ netty-websockets/src/test/scala/spec.scala
@@ -1,37 +1,29 @@
 package unfiltered.netty.websockets
 
 import com.ning.http.client.providers.netty.NettyAsyncHttpProviderConfig
-import unfiltered.request.{ Path => UFPath, _ }
-import tubesocks.{
-  Open => TOpen,
-  Close => TClose,
-  Message => TMessage,
-  Error => TError,
-  _
-}
+import unfiltered.request.{ GET, Path => UFPath }
 import java.net.URI
-import java.util.concurrent.{ CountDownLatch, TimeUnit }
 import scala.collection.mutable
 
 object WebSocketPlanSpec extends unfiltered.spec.netty.Served {
-  def setup = _.handler(Planify({
+  def setup = _.handler(Planify {
     case GET(UFPath("/")) => {
       case Open(s) =>
         s.send("open")
       case Message(s, Text(m)) =>
         s.send(m)
     }
-  }))
+  })
 
   def wsuri = host.to_uri.toString.replace("http", "ws")
 
   "A websocket server" should {
     "accept connections" in {
       var m = mutable.Map.empty[String, String]
-      Sock.uri(wsuri) {
-        case TOpen(s) =>
+      tubesocks.Sock.uri(wsuri) {
+        case tubesocks.Open(s) =>
           s.send("open")
-        case TMessage(t, _) =>
+        case tubesocks.Message(t, _) =>
           m += ("rec" -> t)
       }
       m must havePair(("rec", "open")).eventually
@@ -39,10 +31,10 @@ object WebSocketPlanSpec extends unfiltered.spec.netty.Served {
 
     "handle messages" in {
       var m = mutable.Map.empty[String, String]
-      Sock.uri(wsuri) {
-        case TOpen(s) =>
+      tubesocks.Sock.uri(wsuri) {
+        case tubesocks.Open(s) =>
           s.send("from client")
-        case TMessage(t, _) =>
+        case tubesocks.Message(t, _) =>
           m += ("rec" -> t)
       }
       m must havePair(("rec", "from client")).eventually
