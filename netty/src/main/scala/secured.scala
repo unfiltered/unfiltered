3b774e549256a9abd8e263a365591cb7ae2e8ea8
softprops <d.tangren@gmail.com>
2013-12-16 23:22:42 -0500

    order imports

--- netty/src/main/scala/secured.scala
+++ netty/src/main/scala/secured.scala
@@ -1,8 +1,8 @@
 package unfiltered.netty
 
 import io.netty.channel.{ Channel, ChannelFuture, ChannelFutureListener, ChannelHandlerContext, ChannelInboundHandlerAdapter }
-import io.netty.handler.ssl.{ SslHandler, SslHandshakeCompletionEvent }
 import io.netty.channel.ChannelHandler.Sharable
+import io.netty.handler.ssl.{ SslHandler, SslHandshakeCompletionEvent }
 import io.netty.util.concurrent.{ Future, GenericFutureListener }
 
 /** Adds ssl handshaking to a channel handler's #channelConnected method
@@ -10,7 +10,7 @@ import io.netty.util.concurrent.{ Future, GenericFutureListener }
   * The ssl handshake will be handled for you. You may wish to know preform custom behavior on
   * if the handshake is a pass or failure. To do so, use the handshakeSuccess(ctx) or handshakeFailure(ctx, event)
   * to do so */
-@Sharable // this indicates that the handler is stateless and be called without synchronization
+@Sharable
 trait Secured extends ChannelInboundHandlerAdapter {
   /** Client code should _always_ pass userEventTriggered events upstream */
   override def userEventTriggered(ctx: ChannelHandlerContext, event: java.lang.Object): Unit =
