9bb917b1be7f314569bbc01921f7725e6bc1834f
softprops <d.tangren@gmail.com>
2014-01-01 17:23:43 -0500

    note on nonusefulness

--- netty/src/main/scala/secured.scala
+++ netty/src/main/scala/secured.scala
@@ -9,7 +9,8 @@ import io.netty.util.concurrent.{ Future, GenericFutureListener }
   * This assumes a SslHandler was added to the underlying ChannelPipeline.
   * The ssl handshake will be handled for you. You may wish to know preform custom behavior on
   * if the handshake is a pass or failure. To do so, use the handshakeSuccess(ctx) or handshakeFailure(ctx, event)
-  * to do so */
+  * to do so
+  * note(doug): since the handshake of ssl handlers are not invoked for you. this traits usefulness is a little dubious. users can just override userEventTriggered, matching on netties SslHandshakeCompletionEvent. we may want to remove this trait altogether as it adds little value */
 @Sharable
 trait Secured extends ChannelInboundHandlerAdapter {
   /** Client code should _always_ pass userEventTriggered events upstream */
@@ -23,9 +24,9 @@ trait Secured extends ChannelInboundHandlerAdapter {
     }
 
   /** Called after a successful ssl handshake attempt */
-  def handshakeSuccess(ctx: ChannelHandlerContext): Unit = {}
+  def handshakeSuccess(ctx: ChannelHandlerContext): Unit = println("handshake success")
 
   /** Called after a failed ssl handshake attempt */
-  def handshakeFailure(ctx: ChannelHandlerContext, event: SslHandshakeCompletionEvent): Unit = {}
+  def handshakeFailure(ctx: ChannelHandlerContext, event: SslHandshakeCompletionEvent): Unit = println("handshake failure")
 
 }
