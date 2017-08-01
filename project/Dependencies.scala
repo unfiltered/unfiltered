69d6bc5d626a496f030cc95dee33d6ca0695bfbe
xuwei-k <6b656e6a69@gmail.com>
2017-08-01 12:01:53 +0900

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val servletApiDep = "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
 
   def specs2Dep(sv: String) =
-    "org.specs2" %% "specs2-core" % "3.9.0"
+    "org.specs2" %% "specs2-core" % "3.9.4"
 
   def okHttp = "com.squareup.okhttp3" % "okhttp" % "3.5.0" :: Nil
 
@@ -13,14 +13,14 @@ object Dependencies {
   val commonsCodecVersion = "1.10"
   val scalacheckVersion = "1.13.5"
   val jodaTimeVersion = "2.9.9"
-  val jodaConvertVersion = "1.8.1"
+  val jodaConvertVersion = "1.8.2"
   val scalaXmlVersion = "1.0.6"
   val commonsIoVersion = "2.5"
   val commonsFileUploadVersion = "1.3.2"
   val jettyVersion = "9.2.21.v20170120"
-  val nettyVersion = "4.1.12.Final"
+  val nettyVersion = "4.1.13.Final"
   val scalatestVersion = "3.0.3"
-  val json4sVersion = "3.5.2"
+  val json4sVersion = "3.5.3"
   val asyncHttpClientVersion = "1.8.17"
   val scribeJavaVersion = "3.3.0"
 }
