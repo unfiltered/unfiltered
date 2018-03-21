40c4f08a7c4b91b1e8f70015a2c2569c714fc115
xuwei-k <6b656e6a69@gmail.com>
2018-03-21 10:49:58 +0900

    Scala 2.12.5

--- project/common.scala
+++ project/common.scala
@@ -9,7 +9,7 @@ object Common {
     Nil
   )
 
-  val Scala212 = "2.12.4"
+  val Scala212 = "2.12.5"
 
   val settings: Seq[Setting[_]] = Defaults.coreDefaultSettings ++ Seq(
     organization := "ws.unfiltered",
