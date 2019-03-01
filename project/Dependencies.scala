1e18db44d1d9b60f73a509964f19ff607e4e306c
Scala steward <me@scala-steward.org>
2019-03-01 06:00:49 +0100

    Update specs2-core to 4.3.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val servletApiDep = "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
 
   val specs2Dep = Def.setting {
-    "org.specs2" %% "specs2-core" % "4.3.5"
+    "org.specs2" %% "specs2-core" % "4.3.6"
   }
 
   def okHttp = "com.squareup.okhttp3" % "okhttp" % "3.11.0" :: Nil
