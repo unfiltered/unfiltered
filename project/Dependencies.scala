e92299ca53e3212fdf7393df7c5c82bd873b489d
Scala steward <me@scala-steward.org>
2019-03-01 05:59:58 +0100

    Update commons-fileupload to 1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val scalacheckVersion = "1.14.0"
   val scalaXmlVersion = "1.1.1"
   val commonsIoVersion = "2.6"
-  val commonsFileUploadVersion = "1.3.3"
+  val commonsFileUploadVersion = "1.4"
   val jettyVersion = "9.4.14.v20181114"
   val nettyVersion = "4.1.13.Final" // TODO https://github.com/unfiltered/unfiltered/issues/414
   val scalatestVersion = Def.setting {
