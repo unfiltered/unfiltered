fc5efce275b1095b589336fe3d9b411be1181ccc
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-03-17 04:20:04 +0100

    Update okhttp to 3.14.0 (#439)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     "org.specs2" %% "specs2-core" % "4.5.1"
   }
 
-  def okHttp = "com.squareup.okhttp3" % "okhttp" % "3.13.1" :: Nil
+  def okHttp = "com.squareup.okhttp3" % "okhttp" % "3.14.0" :: Nil
 
   def integrationTestDeps = Def.setting((specs2Dep.value :: okHttp) map { _ % "test" })
 
