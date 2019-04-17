8e0fe971bdba3e52993efa198951a00b8d5a2bf3
Scala steward <me@scala-steward.org>
2019-04-17 13:55:25 +0200

    Update netty-codec-http, netty-handler, ... to 4.1.35.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val commonsIoVersion = "2.6"
   val commonsFileUploadVersion = "1.4"
   val jettyVersion = "9.4.16.v20190411"
-  val nettyVersion = "4.1.13.Final" // TODO https://github.com/unfiltered/unfiltered/issues/414
+  val nettyVersion = "4.1.35.Final" // TODO https://github.com/unfiltered/unfiltered/issues/414
   val scalatestVersion = "3.0.8-RC2"
   val json4sVersion = "3.6.5"
   val asyncHttpClientVersion = "1.8.17"
