fef561ada56eb1880d66f77e270963dc87e00ed9
xuwei-k <6b656e6a69@gmail.com>
2020-04-23 17:47:05 +0900

    Scala 2.13.2

--- project/common.scala
+++ project/common.scala
@@ -20,7 +20,7 @@ object Common {
   val settings: Seq[Setting[_]] = Defaults.coreDefaultSettings ++ Seq(
     organization := "ws.unfiltered",
 
-    crossScalaVersions := Seq("2.13.1", Scala212, "2.11.12"),
+    crossScalaVersions := Seq("2.13.2", Scala212, "2.11.12"),
 
     scalaVersion := Scala212,
 
