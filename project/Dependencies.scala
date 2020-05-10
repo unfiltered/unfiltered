6c0292a1f0df7567b1bcad611a6e99898d1e3dc9
Scala Steward <me@scala-steward.org>
2020-05-10 19:17:37 +0200

    Update scalatest to 3.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val commonsFileUploadVersion = "1.4"
   val jettyVersion = "9.4.28.v20200408"
   val nettyVersion = "4.1.49.Final"
-  val scalatestVersion = "3.1.1"
+  val scalatestVersion = "3.1.2"
   val scalatestScalacheckVersion = "3.1.1.1"
   val json4sVersion = "3.6.8"
   val asyncHttpClientVersion = "1.8.17"
