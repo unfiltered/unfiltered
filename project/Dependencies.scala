91eba9a2410925757c1852fde3c0e3454baa849f
Scala Steward <me@scala-steward.org>
2020-06-15 17:24:50 +0200

    Update json4s-native to 3.6.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val nettyVersion = "4.1.50.Final"
   val scalatestVersion = "3.1.2"
   val scalatestScalacheckVersion = "3.1.2.0"
-  val json4sVersion = "3.6.8"
+  val json4sVersion = "3.6.9"
   val asyncHttpClientVersion = "1.8.17"
   val javaxActivationVersion = "1.1.1"
 }
