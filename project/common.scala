483296f71cb90e7f34ef098f893672b334394d34
xuwei-k <6b656e6a69@gmail.com>
2020-07-19 15:36:31 +0900

    Scala 2.12.12

--- project/common.scala
+++ project/common.scala
@@ -15,7 +15,7 @@ object Common {
     }
   )
 
-  val Scala212 = "2.12.11"
+  val Scala212 = "2.12.12"
 
   val settings: Seq[Setting[_]] = Defaults.coreDefaultSettings ++ Seq(
     organization := "ws.unfiltered",
