d831ed06524cc087c48dfbefc329966bde013b9d
Scala Steward <me@scala-steward.org>
2020-08-22 09:00:48 +0200

    Update scalacheck-1-14 to 3.2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val jettyVersion = "9.4.31.v20200723"
   val nettyVersion = "4.1.51.Final"
   val scalatestVersion = "3.2.1"
-  val scalatestScalacheckVersion = "3.2.1.0"
+  val scalatestScalacheckVersion = "3.2.2.0"
   val json4sVersion = "3.6.9"
   val asyncHttpClientVersion = "1.8.17"
   val javaxActivationVersion = "1.1.1"
