19c49355a9cb59e4aac4400290dd0229f398b5a0
Scala Steward <me@scala-steward.org>
2020-09-01 17:43:34 +0200

    Update commons-codec to 1.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   def integrationTestDeps = Def.setting((specs2Dep.value :: okHttp) map { _ % "test" })
 
-  val commonsCodecVersion = "1.14"
+  val commonsCodecVersion = "1.15"
   val scalaXmlVersion = "1.3.0"
   val commonsIoVersion = "2.7"
   val commonsFileUploadVersion = "1.4"
