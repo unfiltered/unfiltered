8a865932c32a9e512e7904a24fe6a2c4d19bf7a1
Scala Steward <me@scala-steward.org>
2021-06-09 01:13:10 +0200

    Update jetty-webapp to 11.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val scalaXmlVersion = "2.0.0"
   val commonsIoVersion = "2.9.0"
   val commonsFileUploadVersion = "1.4"
-  val jettyVersion = "11.0.3"
+  val jettyVersion = "11.0.4"
   val nettyVersion = "4.1.65.Final"
   val scalatestVersion = "3.2.9"
   val scalatestScalacheckVersion = "3.2.9.0"
