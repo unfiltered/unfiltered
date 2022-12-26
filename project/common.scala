cfd88d92847a2f1f896bc1b727bedd91e537558b
xuwei-k <6b656e6a69@gmail.com>
2022-12-27 08:25:20 +0900

    remove some scalacOptions

--- project/common.scala
+++ project/common.scala
@@ -32,15 +32,6 @@ object Common {
     }.toList.flatten,
 
     scalacOptions ++= PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion.value)){
-      case Some((3, _)) =>
-        Seq(
-          "-Xignore-scala2-macros",
-          "-source",
-          "3.0-migration",
-        )
-    }.toList.flatten,
-
-    scalacOptions ++= PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion.value)){
       case Some((2, v)) if v <= 12 =>
         Seq("-Ywarn-adapted-args", "-Xfuture")
     }.toList.flatten,
