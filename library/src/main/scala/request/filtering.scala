85ae46ecaf97c0d85a7d7d8f7c7a848f94769a28
xuwei-k <6b656e6a69@gmail.com>
2022-12-27 16:04:47 +0900

    fix warnings

--- library/src/main/scala/request/filtering.scala
+++ library/src/main/scala/request/filtering.scala
@@ -6,12 +6,12 @@ import java.util.zip.{GZIPInputStream => GZIS}
 /** Apply an input stream filter to a request input stream. */
 object RequestFilter {
   abstract class Filtering[S <: InputStream, +T](req: HttpRequest[T]) extends DelegatingRequest(req) {
-      private def charset = Charset(this).getOrElse { "iso-8859-1" }
+    private def charset = Charset(this).getOrElse { "iso-8859-1" }
 
-      override lazy val inputStream = filter(delegate.inputStream)
+    override lazy val inputStream = filter(delegate.inputStream)
 
-      override lazy val reader =
-        new BufferedReader(new InputStreamReader(inputStream, charset))
+    override lazy val reader =
+      new BufferedReader(new InputStreamReader(inputStream, charset))
 
     def filter(is: InputStream): S
   }
