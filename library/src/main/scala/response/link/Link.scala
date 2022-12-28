7d4ec7db1f17abdfb9eaac7c556479eab1cfbd2d
xuwei-k <6b656e6a69@gmail.com>
2022-12-28 13:59:25 +0900

    Update RFC url
    (cherry picked from commit 98775d92a48d4dfc6bcc239f09183d19b9ad6058)

--- library/src/main/scala/response/link/Link.scala
+++ library/src/main/scala/response/link/Link.scala
@@ -3,7 +3,7 @@ package unfiltered.response.link
 import unfiltered.response.{HttpResponse, Responder}
 
 /** Link header implementation as specified in
-    [[http://tools.ietf.org/html/rfc5988 rfc5988]]. */
+    [[https://www.rfc-editor.org/rfc/rfc5988 rfc5988]]. */
 object Link {
   def apply(refs: Ref*): Responder[Any] =
     (res: HttpResponse[Any]) => {
