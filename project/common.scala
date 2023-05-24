b5b791e5858c744f1389958664c607e14fa36954
kenji yoshida <6b656e6a69@gmail.com>
2023-05-25 06:54:46 +0900

    Scala 3.3.0

--- project/common.scala
+++ project/common.scala
@@ -19,7 +19,7 @@ object Common {
   val settings: Seq[Setting[_]] = Def.settings(
     organization := "ws.unfiltered",
 
-    crossScalaVersions := Seq(Scala213, "3.2.2"),
+    crossScalaVersions := Seq(Scala213, "3.3.0"),
 
     scalaVersion := Scala213,
 
