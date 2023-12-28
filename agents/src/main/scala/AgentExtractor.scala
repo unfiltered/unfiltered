e58931ad0ad202acc8c6b4bc259894f747e87cc2
xuwei-k <6b656e6a69@gmail.com>
2023-12-28 17:21:27 +0900

    use new wildcard type syntax

--- agents/src/main/scala/AgentExtractor.scala
+++ agents/src/main/scala/AgentExtractor.scala
@@ -1,18 +1,18 @@
 package unfiltered.request
 
 /**
- * AgentExtractor instances provide a predicate (HttpRequest[_] => Boolean)
+ * AgentExtractor instances provide a predicate (HttpRequest[?] => Boolean)
  * for ad-hoc tests, a standard Unfiltered request extractor, and a means
  * for composing simple extractors into more complex ones.
  */
-trait AgentExtractor extends (HttpRequest[_] => Boolean) {
+trait AgentExtractor extends (HttpRequest[?] => Boolean) {
   /** Predicate for an agent string. */
   val test: String => Boolean
   /** Composition (f.g). */
   def &(e: AgentExtractor) = new AgentExtractor {
     val test = (ua: String) => AgentExtractor.this.test(ua) && e.test(ua)
   }
-  def apply(req: HttpRequest[_]) =
+  def apply(req: HttpRequest[?]) =
     UserAgent.unapply(req).exists(test)
   def unapply[A](req: HttpRequest[A]) =
     UserAgent.unapply(req).collect { case ua if test(ua) => Some(req) }
