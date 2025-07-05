65de6a3b187e8c7b1e8b9c381c159f729755bbc7
xuwei-k <6b656e6a69@gmail.com>
2025-07-05 17:26:48 +0900

    127.0.0.1 => 0:0:0:0:0:0:0:1

--- netty/src/test/scala/ServerSpec.scala
+++ netty/src/test/scala/ServerSpec.scala
@@ -36,7 +36,7 @@ class ServerSpec extends Specification with unfiltered.specs2.netty.Served {
       http(req(host)).as_string must_== "test"
     }
     "provide a remote address" in {
-      http(req(host / "addr")).as_string must_== "127.0.0.1"
+      http(req(host / "addr")).as_string must_== "0:0:0:0:0:0:0:1"
     }
     "provide a remote address accounting for X-Forwarded-For header" in {
       http(
@@ -44,7 +44,9 @@ class ServerSpec extends Specification with unfiltered.specs2.netty.Served {
       ).as_string must_== "66.108.150.228"
     }
     "provide a remote address accounting for X-Forwarded-For header filtering private addresses" in {
-      http(req(host / "addr_extractor") <:< Map("X-Forwarded-For" -> "172.31.255.255")).as_string must_== "127.0.0.1"
+      http(
+        req(host / "addr_extractor") <:< Map("X-Forwarded-For" -> "172.31.255.255")
+      ).as_string must_== "0:0:0:0:0:0:0:1"
     }
     "respond to requests in ustream channel plan" in {
       http(req(host / "planc")).as_string must_== "planc"
