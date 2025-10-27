30f2fefb1f2c7967378dd68973081740e88a06fb
Scala Steward <scala_steward@virtuslab.com>
2025-10-27 20:49:13 +0000

    Update json4s-native-core to 4.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val nettyVersion = "4.2.7.Final"
   val scalatestVersion = "3.2.19"
   val scalatestScalacheckVersion = s"${scalatestVersion}.0"
-  val json4sVersion = "4.0.7"
+  val json4sVersion = "4.1.0"
   val asyncHttpClientVersion = "1.8.17"
   val activationVersion = "2.1.4"
 }
