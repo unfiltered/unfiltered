9907038d51455544919f89ff60fa6a57bc3233d0
xuwei-k <6b656e6a69@gmail.com>
2026-01-05 07:31:12 +0900

    Delete JIteratorWrapper

--- filter-uploads/src/main/scala/request/conversions.scala
+++ filter-uploads/src/main/scala/request/conversions.scala
@@ -4,13 +4,6 @@ object IteratorConversions {
   import org.apache.commons.{fileupload => fu}
   import fu.FileItemIterator
   import fu.FileItemStream
-  import java.util.{Iterator => JIterator}
-
-  /** convert java iterator to scala iterator */
-  implicit final class JIteratorWrapper[A](i: JIterator[A]) extends Iterator[A] {
-    def hasNext: Boolean = i.hasNext
-    def next(): A = i.next
-  }
 
   /** convert apache commons file iterator to scala iterator */
   implicit final class FIIteratorWrapper(i: FileItemIterator) extends Iterator[FileItemStream] {
